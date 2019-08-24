1b0984da9d27e0430d1b96eb95e5164d68a35946
Jürgen Hörmann <j.hoermann77@gmail.com>
2019-08-24 20:09:54 +0200

    updated globals

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -2,7 +2,7 @@ library jvx_mobile_v3.globals;
 
 String appName = 'demo';
 String baseUrl = 'http://172.16.0.16:8080/JVx.mobile/services/mobile';
-//String baseUrl = 'http://192.168.43.37:8080/JVx.mobile/services/mobile';
+//String baseUrl = 'http://192.168.0.10:8080/JVx.mobile/services/mobile';
 String language = 'de';
 String clientId;
 String jsessionId;
