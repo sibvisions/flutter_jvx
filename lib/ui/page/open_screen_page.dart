0f0bd40317bbb2fd5beccab690969dc15711c8af
Jürgen Hörmann <j.hoermann77@gmail.com>
2019-10-21 12:31:13 +0200

    Screen  needs to know if an errror happens

--- lib/ui/page/open_screen_page.dart
+++ lib/ui/page/open_screen_page.dart
@@ -99,8 +99,8 @@ class _OpenScreenPageState extends State<OpenScreenPage>
 
       if (isScreenRequest(state.requestType) &&
           //state.screenGeneric != null &&
-          !state.loading &&
-          !state.error) {
+          !state.loading //&& !state.error
+          ) {
 
         if (state.requestType == RequestType.OPEN_SCREEN) {
           if (mounted && _scaffoldKey.currentState != null && _scaffoldKey.currentState.isEndDrawerOpen)
