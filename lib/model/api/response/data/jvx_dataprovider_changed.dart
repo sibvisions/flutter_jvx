6bae16ec3326134a9884665267e2f2770f1a3cb4
Jürgen Hörmann <j.hoermann77@gmail.com>
2020-03-23 11:36:57 +0100

    Added columnNames to dataproviderChanged

--- lib/model/api/response/data/jvx_dataprovider_changed.dart
+++ lib/model/api/response/data/jvx_dataprovider_changed.dart
@@ -4,12 +4,14 @@ class JVxDataproviderChanged extends ResponseObject {
   String dataProvider;
   int reload;
   int selectedRow;
+  List<String> columnNames;
 
-  JVxDataproviderChanged({this.dataProvider, this.reload, this.selectedRow});
+  JVxDataproviderChanged({this.dataProvider, this.reload, this.selectedRow, this.columnNames});
 
   JVxDataproviderChanged.fromJson(Map<String, dynamic> json) {
     dataProvider = json['dataProvider'];
     reload = json['reload'];
     selectedRow = json['selectedRow'];
+    columnNames = json['columnNames'];
   }
 }
