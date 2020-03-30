6da2bf97cbc03e6f0b5178694893f64660bcef4f
Jürgen Hörmann <j.hoermann77@gmail.com>
2020-03-30 10:53:27 +0200

    Fixed remove focus on textfields if button pressed

--- lib/ui/component/jvx_button.dart
+++ lib/ui/component/jvx_button.dart
@@ -18,7 +18,7 @@ class JVxButton extends JVxActionComponent {
   String text = "";
   Widget icon;
 
-  JVxButton(GlobalKey componentId, BuildContext context) : super(componentId, context);
+  JVxButton(Key componentId, BuildContext context) : super(componentId, context);
 
   @override
   void updateProperties(ChangedComponent changedComponent) {
