5d16149dcb0b1929640e6c7feb5f88e673634fbb
Jürgen Hörmann <j.hoermann77@gmail.com>
2020-05-13 11:49:23 +0200

    Dynamically change button width and padding, wenn preferred size width smaller then ButtonTheme minWidth

--- lib/ui/component/jvx_button.dart
+++ lib/ui/component/jvx_button.dart
@@ -18,33 +18,37 @@ class JVxButton extends JVxActionComponent {
   String text = "";
   Widget icon;
 
-  JVxButton(Key componentId, BuildContext context) : super(componentId, context);
+  JVxButton(Key componentId, BuildContext context)
+      : super(componentId, context);
 
   @override
   void updateProperties(ChangedComponent changedComponent) {
     super.updateProperties(changedComponent);
     text = changedComponent.getProperty<String>(ComponentProperty.TEXT, text);
-    String image = changedComponent.getProperty<String>(ComponentProperty.IMAGE);
-    if (image!=null) {
-      if(checkFontAwesome(image)) {
+    String image =
+        changedComponent.getProperty<String>(ComponentProperty.IMAGE);
+    if (image != null) {
+      if (checkFontAwesome(image)) {
         icon = convertFontAwesomeTextToIcon(image, UIData.textColor);
       } else {
         List strinArr = List<String>.from(image.split(','));
         File file = File('${globals.dir}${strinArr[0]}');
         if (file.existsSync()) {
-          Size size = Size(16,16);
+          Size size = Size(16, 16);
 
-        if (strinArr.length>=3 && double.tryParse(strinArr[1])!=null && double.tryParse(strinArr[2])!=null) 
-          size = Size(double.parse(strinArr[1]), double.parse(strinArr[2]));
-        icon = Image.memory(
-          file.readAsBytesSync(),
-          width: size.width,
-          height: size.height,
-        );
+          if (strinArr.length >= 3 &&
+              double.tryParse(strinArr[1]) != null &&
+              double.tryParse(strinArr[2]) != null)
+            size = Size(double.parse(strinArr[1]), double.parse(strinArr[2]));
+          icon = Image.memory(
+            file.readAsBytesSync(),
+            width: size.width,
+            height: size.height,
+          );
 
-        BlocProvider.of<ApiBloc>(context)
-            .dispatch(Reload(requestType: RequestType.RELOAD));
-      }
+          BlocProvider.of<ApiBloc>(context)
+              .dispatch(Reload(requestType: RequestType.RELOAD));
+        }
       }
     }
   }
@@ -53,7 +57,8 @@ class JVxButton extends JVxActionComponent {
     TextUtils.unfocusCurrentTextfield(context);
 
     Future.delayed(const Duration(milliseconds: 100), () {
-      PressButton pressButton = PressButton(jvxAction.Action(componentId: this.name, label: this.text));
+      PressButton pressButton = PressButton(
+          jvxAction.Action(componentId: this.name, label: this.text));
       BlocProvider.of<ApiBloc>(context).dispatch(pressButton);
     });
   }
@@ -61,36 +66,42 @@ class JVxButton extends JVxActionComponent {
   @override
   Widget getWidget() {
     Widget child;
-    Widget textWidget = new Text(text!=null?text:"", 
-      style: TextStyle(
-        fontSize: style.fontSize,
-        color: UIData.textColor
-      )
-    );
+    Widget textWidget = new Text(text != null ? text : "",
+        style: TextStyle(fontSize: style.fontSize, color: UIData.textColor));
 
     if (text?.isNotEmpty ?? true) {
-      if (icon!=null) {
-        child = Row(children: <Widget>[icon, SizedBox(width: 10), textWidget], mainAxisAlignment: MainAxisAlignment.center,);
+      if (icon != null) {
+        child = Row(
+          children: <Widget>[icon, SizedBox(width: 10), textWidget],
+          mainAxisAlignment: MainAxisAlignment.center,
+        );
       } else {
         child = textWidget;
       }
-    } else if (icon!=null) {
+    } else if (icon != null) {
       child = icon;
     } else {
       child = textWidget;
     }
 
-    return 
-    ButtonTheme(
-  minWidth: 44,
-  child: 
-      RaisedButton(
-        key: this.componentId, 
-        onPressed: this.enabled?buttonPressed:null,
-        color: UIData.ui_kit_color_2[400],
-        elevation: 10,
-        child: child,
-        splashColor: this.background,
-      ));
+    double minWidth = 44;
+    EdgeInsets padding;
+
+    if (this.isPreferredSizeSet && this.preferredSize.width < minWidth) {
+      padding = EdgeInsets.symmetric(horizontal: 0);
+      minWidth = this.preferredSize.width;
+    }
+
+    return ButtonTheme(
+        minWidth: minWidth,
+        padding: padding,
+        child: RaisedButton(
+          key: this.componentId,
+          onPressed: this.enabled ? buttonPressed : null,
+          color: UIData.ui_kit_color_2[400],
+          elevation: 10,
+          child: child,
+          splashColor: this.background,
+        ));
   }
 }
