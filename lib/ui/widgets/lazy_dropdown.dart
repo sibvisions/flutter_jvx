c75842e09a5c51682d536242fc23cc310e7aafcf
Jürgen Hörmann <j.hoermann77@gmail.com>
2020-05-26 17:57:58 +0200

    Refactored linkedCellEditor

--- lib/ui/widgets/lazy_dropdown.dart
+++ lib/ui/widgets/lazy_dropdown.dart
@@ -5,6 +5,7 @@ import '../../model/api/response/data/jvx_data.dart';
 import '../../ui/screen/component_data.dart';
 import '../../utils/translations.dart';
 import '../../utils/uidata.dart';
+import '../../utils/globals.dart' as globals;
 
 class LazyDropdown extends StatefulWidget {
   final allowNull;
@@ -18,8 +19,7 @@ class LazyDropdown extends StatefulWidget {
   final List<String> displayColumnNames;
 
   LazyDropdown(
-      {
-      @required this.allowNull,
+      {@required this.allowNull,
       @required this.context,
       @required this.data,
       this.displayColumnNames,
@@ -27,8 +27,8 @@ class LazyDropdown extends StatefulWidget {
       this.onCancel,
       this.onScrollToEnd,
       this.onFilter,
-      this.fetchMoreYOffset = 0}) :
-        assert(allowNull != null),
+      this.fetchMoreYOffset = 0})
+      : assert(allowNull != null),
         assert(context != null),
         assert(data != null);
 
@@ -93,12 +93,12 @@ class _LazyDropdownState extends State<LazyDropdown> {
 
   Widget itemBuilder(BuildContext ctxt, int index) {
     List<Widget> children = new List<Widget>();
-    List<dynamic> row = widget.data.data.getRow(index, widget.displayColumnNames);
+    List<dynamic> row =
+        widget.data.data.getRow(index, widget.displayColumnNames);
 
-    if (row!=null) {
+    if (row != null) {
       row.forEach((c) {
-        children.add(getTableColumn(
-              c != null ? c.toString() : "", index));
+        children.add(getTableColumn(c != null ? c.toString() : "", index));
       });
 
       return getTableRow(children, index, false);
@@ -117,14 +117,15 @@ class _LazyDropdownState extends State<LazyDropdown> {
           child: ListTile(title: Row(children: children)));
     } else {
       return Container(
-          decoration: BoxDecoration(
-            boxShadow: [BoxShadow(color: Colors.grey[400], spreadRadius: 1)],
-            color: Colors.white,
-          ),
-          child: ListTile(
-            title: Row(children: children),
-            onTap: () => _onRowTapped(index),
-          ));
+          child: Card(
+              color: Colors.white
+                  .withOpacity(globals.applicationStyle.menuOpacity),
+              elevation: 2.0,
+              child: ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Row(children: children),
+                onTap: () => _onRowTapped(index),
+              )));
     }
   }
 
@@ -134,10 +135,8 @@ class _LazyDropdownState extends State<LazyDropdown> {
     return Expanded(
         flex: flex,
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-              child: Text(text),
-              padding: EdgeInsets.all(5)),
+          padding: const EdgeInsets.only(left: 16.0, right: 16),
+          child: Container(child: Text(text), padding: EdgeInsets.all(0)),
         ));
   }
 
@@ -151,11 +150,14 @@ class _LazyDropdownState extends State<LazyDropdown> {
 
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    ColorScheme colorScheme = theme.colorScheme;
     int itemCount = 0;
     JVxData data = widget.data.data;
     if (data != null && data.records != null) itemCount = data.records.length;
 
     return Dialog(
+        insetPadding: EdgeInsets.fromLTRB(25, 25, 25, 25),
         elevation: 0.0,
         backgroundColor: Colors.transparent,
         child: Container(
@@ -166,33 +168,45 @@ class _LazyDropdownState extends State<LazyDropdown> {
             child: Column(
               children: <Widget>[
                 Container(
-                  color: UIData.ui_kit_color_2,
+                    color: UIData.ui_kit_color_2,
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                            child: Text(
+                              Translations.of(context)
+                                  .text2("Select item", 'Select item')
+                                  .toUpperCase(),
+                              style: TextStyle(
+                                  color:
+                                      colorScheme.brightness == Brightness.light
+                                          ? colorScheme.onPrimary
+                                          : colorScheme.onSurface),
+                            )),
+                      ],
+                    )),
+                Container(
                     child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      hintText: Translations.of(context).text2("Search"),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: UIData.ui_kit_color_2, width: 1.0)),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: UIData.ui_kit_color_2, width: 0.0)),
-                    ),
-                    key: widget.key,
-                    controller: _controller,
-                    maxLines: 1,
-                    keyboardType: TextInputType.text,
-                    onChanged: startTimerValueChanged,
-                    focusNode: node,
-                  ),
-                )),
-
+                        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                        child: TextFormField(
+                          key: widget.key,
+                          controller: _controller,
+                          maxLines: 1,
+                          keyboardType: TextInputType.text,
+                          onChanged: startTimerValueChanged,
+                          style: new TextStyle(
+                              fontSize: 14.0, color: Colors.black),
+                          decoration: new InputDecoration(
+                              hintStyle: TextStyle(color: Colors.green),
+                              labelText: Translations.of(context)
+                                  .text2("Search", 'Search'),
+                              labelStyle: TextStyle(
+                                  fontSize: 14.0, fontWeight: FontWeight.w600)),
+                        ))),
                 Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+                    padding:
+                        const EdgeInsets.only(left: 16, right: 16, top: 10),
                     child: ListView.builder(
                       controller: _scrollController,
                       itemCount: itemCount,
@@ -200,24 +214,21 @@ class _LazyDropdownState extends State<LazyDropdown> {
                     ),
                   ),
                 ),
-
-                ButtonBar(
-                    alignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      new RaisedButton(
-                        child: Text(Translations.of(context).text2("Clear")),
-                        onPressed: _onDelete,
-                        color: UIData.ui_kit_color_2[200],
-                      ),
-                      new RaisedButton(
-                        child: Text(
-                          Translations.of(context).text2("Cancel"),
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        onPressed: _onCancel,
-                        color: UIData.ui_kit_color_2,
-                      ),
-                    ]),
+                ButtonBar(alignment: MainAxisAlignment.end, children: <Widget>[
+                  new FlatButton(
+                    child: Text(
+                        Translations.of(context).text2("Clear").toUpperCase()),
+                    onPressed: _onDelete,
+                    //color: UIData.ui_kit_color_2[200],
+                  ),
+                  new FlatButton(
+                    child: Text(
+                      Translations.of(context).text2("Cancel").toUpperCase(),
+                    ),
+                    onPressed: _onCancel,
+                    //color: UIData.ui_kit_color_2,
+                  ),
+                ]),
               ],
             ),
           ),
