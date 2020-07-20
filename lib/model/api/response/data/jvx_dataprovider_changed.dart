aa28fae810f6b4000a5f364c6b844bfdd58d9f7d
Jürgen Hörmann <j.hoermann77@gmail.com>
2020-07-20 11:13:30 +0200

    Updated Models meta_data and datadprovider_changed

--- lib/model/api/response/data/jvx_dataprovider_changed.dart
+++ lib/model/api/response/data/jvx_dataprovider_changed.dart
@@ -5,13 +5,22 @@ class JVxDataproviderChanged extends ResponseObject {
   int reload;
   int selectedRow;
   List<String> columnNames;
+  bool readOnly;
+  bool deleteEnabled;
+  bool updateEnabled;
+  bool insertEnabled;
 
-  JVxDataproviderChanged({this.dataProvider, this.reload, this.selectedRow, this.columnNames});
+  JVxDataproviderChanged(
+      {this.dataProvider, this.reload, this.selectedRow, this.columnNames});
 
   JVxDataproviderChanged.fromJson(Map<String, dynamic> json) {
     dataProvider = json['dataProvider'];
     reload = json['reload'];
     selectedRow = json['selectedRow'];
     columnNames = json['columnNames'];
+    readOnly = json['readOnly'];
+    deleteEnabled = json['deleteEnabled'];
+    updateEnabled = json['updateEnabled'];
+    insertEnabled = json['insertEnabled'];
   }
 }
--- lib/model/api/response/meta_data/jvx_meta_data.dart
+++ lib/model/api/response/meta_data/jvx_meta_data.dart
@@ -5,7 +5,9 @@ import 'jvx_meta_data_data_provider.dart';
 
 class JVxMetaData extends ResponseObject {
   String dataProvider;
-  List<JVxMetaDataDataProvider> detailDataProviders = <JVxMetaDataDataProvider>[];
+  List<JVxMetaDataDataProvider> detailDataProviders =
+      <JVxMetaDataDataProvider>[];
+  bool readOnly;
   bool deleteEnabled;
   bool updateEnabled;
   bool insertEnabled;
@@ -13,21 +15,30 @@ class JVxMetaData extends ResponseObject {
   List<String> primaryKeyColumns = <String>[];
   List<String> tableColumnView;
 
-  JVxMetaData({this.dataProvider, this.columns, this.detailDataProviders, this.deleteEnabled, this.updateEnabled});
+  JVxMetaData(
+      {this.dataProvider,
+      this.columns,
+      this.detailDataProviders,
+      this.deleteEnabled,
+      this.updateEnabled});
 
   JVxMetaData.fromJson(Map<String, dynamic> json) {
     dataProvider = json['dataProvider'];
-    if (json['detailDataProviders'] != null) 
-      json['detailDataProviders'].forEach((dp) => detailDataProviders.add(JVxMetaDataDataProvider.fromJson(dp)));
+    if (json['detailDataProviders'] != null)
+      json['detailDataProviders'].forEach((dp) =>
+          detailDataProviders.add(JVxMetaDataDataProvider.fromJson(dp)));
+    readOnly = json['readOnly'];
     deleteEnabled = json['deleteEnabled'];
     updateEnabled = json['updateEnabled'];
     insertEnabled = json['insertEnabled'];
-    if (json['primaryKeyColumns'] != null) 
+    if (json['primaryKeyColumns'] != null)
       primaryKeyColumns = List<String>.from(json['primaryKeyColumns']);
-    if (json['columns'] != null) 
-      json['columns'].forEach((c) => columns.add(JVxMetaDataColumn.fromJson(c)));
+    if (json['columns'] != null)
+      json['columns']
+          .forEach((c) => columns.add(JVxMetaDataColumn.fromJson(c)));
     super.name = json['name'];
     super.componentId = json['componentId'];
-    if (json['columnView.table'] != null) tableColumnView = List<String>.from(json['columnView.table']);
+    if (json['columnView.table'] != null)
+      tableColumnView = List<String>.from(json['columnView.table']);
   }
 }
