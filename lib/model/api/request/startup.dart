c062967a898aecb7c0873532d7d4ae160b9a1edf
Jürgen Hörmann <j.hoermann77@gmail.com>
2020-07-23 09:19:25 +0200

    Added technology to startup request

--- lib/model/api/request/startup.dart
+++ lib/model/api/request/startup.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:universal_io/prefer_universal/io.dart';
+
 import '../../../model/api/request/request.dart';
 
 /// Request for the [Startup] request.
@@ -13,33 +16,39 @@ class Startup extends Request {
   String userName;
   String password;
   String url;
+  String technology;
 
-  Startup({this.applicationName, 
-          this.authKey, 
-          this.layoutMode, 
-          this.screenWidth, 
-          this.screenHeight, 
-          this.appMode, 
-          this.readAheadLimit, 
-          this.deviceId, 
-          String clientId, 
-          this.userName, 
-          this.password, 
-          this.url,
-          RequestType requestType})
-    : super(clientId: clientId, requestType: requestType);
+  Startup(
+      {this.applicationName,
+      this.authKey,
+      this.layoutMode,
+      this.screenWidth,
+      this.screenHeight,
+      this.appMode,
+      this.readAheadLimit,
+      this.deviceId,
+      String clientId,
+      this.userName,
+      this.password,
+      this.url,
+      RequestType requestType})
+      : super(clientId: clientId, requestType: requestType) {
+    this.technology = "FlutterMobile";
+    if (kIsWeb) this.technology = "FlutterWeb";
+  }
 
   Map<String, dynamic> toJson() => {
-    'applicationName': applicationName,
-    'authKey': authKey,
-    'layoutMode': layoutMode,
-    'screenWidth': screenWidth,
-    'screenHeight': screenHeight,
-    'appMode': appMode,
-    'readAheadLimit': readAheadLimit,
-    'deviceId': deviceId,
-    'userName': userName,
-    'password': password,
-    'url': url
-  };
+        'applicationName': applicationName,
+        'authKey': authKey,
+        'layoutMode': layoutMode,
+        'screenWidth': screenWidth,
+        'screenHeight': screenHeight,
+        'appMode': appMode,
+        'readAheadLimit': readAheadLimit,
+        'deviceId': deviceId,
+        'userName': userName,
+        'password': password,
+        'url': url,
+        'technology': technology
+      };
 }
