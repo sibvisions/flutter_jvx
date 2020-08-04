93458b1b0ab0261f3cdbc9e95e7f27b6ef142a29
Lorenz Mueksch <lorenz.mueksch@sibvisions.com>
2020-08-04 09:57:22 +0200

    added export files for better useability

---
+++ lib/jvx_flutterclient.dart
@@ -0,0 +1,6 @@
+library jvx_flutterclient;
+
+export 'custom_screen/export_custom_screen.dart';
+export 'utils/export_utils.dart';
+export 'ui/export_ui.dart';
+export 'model/export_model.dart';
---
+++ lib/model/export_model.dart
@@ -0,0 +1,7 @@
+library model;
+
+export 'api/response/user_data.dart';
+export 'api/request/request.dart';
+export 'api/response/response_data.dart';
+export 'so_action.dart';
+export 'menu_item.dart';
