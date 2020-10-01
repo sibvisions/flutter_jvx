b6277400060fcca3dd919fa4f2c4d9db2b4164e7
Lorenz Mueksch <lorenz.mueksch@sibvisions.com>
2020-10-01 09:56:24 +0200

    fixes

--- lib/ui/widgets/login_card.dart
+++ lib/ui/widgets/login_card.dart
@@ -178,8 +178,8 @@ class _LoginCardState extends State<LoginCard>
   initState() {
     super.initState();
     username = widget.username;
-    controller =
-        new AnimationController(duration: new Duration(milliseconds: 1500));
+    controller = new AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 1500));
     animation = new Tween(begin: 0.0, end: 1.0).animate(
         new CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn));
     animation.addListener(() => this.setState(() {}));
