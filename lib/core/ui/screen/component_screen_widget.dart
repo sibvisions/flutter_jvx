94a474d3839c82727cc28f98481d7ceca99f7f32
Lorenz Müksch <lorenz.mueksch@sibvisions.com>
2020-11-24 11:27:03 +0100

    another bug fix

--- lib/core/ui/screen/component_screen_widget.dart
+++ lib/core/ui/screen/component_screen_widget.dart
@@ -389,7 +389,7 @@ class ComponentScreenWidgetState extends State<ComponentScreenWidget>
     if (widget.headerComponent != null || widget.footerComponent != null) {
       ComponentWidget headerFooterPanel = CoPanelWidget(
           componentModel:
-              ComponentModel(null));
+              ContainerComponentModel());
       (headerFooterPanel.componentModel as ContainerComponentModel)
           .toUpdateLayout
           .add('BorderLayout,0,0,0,0,0,0,');
