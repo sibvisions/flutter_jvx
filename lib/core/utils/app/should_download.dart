3c0e0d438965f87f533b0624f87c5a0ee15c4bf9
Lorenz Müksch <lorenz.mueksch@sibvisions.com>
2020-12-03 15:31:54 +0100

    removed testing should download

--- lib/core/utils/app/should_download.dart
+++ lib/core/utils/app/should_download.dart
@@ -13,8 +13,6 @@ Future<bool> shouldDownload(AppState appState) async {
     return true;
   }
 
-  return true;
-
   if (Platform.isIOS) {
     _baseDirectory = (await getApplicationSupportDirectory()).path;
   } else {
