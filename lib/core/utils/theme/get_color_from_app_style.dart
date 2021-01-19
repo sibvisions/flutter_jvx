e2cbaf5a951cfdae08ebf54e1595dc86e27261a3
Lorenz Müksch <lorenz.mueksch@sibvisions.com>
2021-01-19 15:03:18 +0100

    changed color bug

--- lib/core/utils/theme/get_color_from_app_style.dart
+++ lib/core/utils/theme/get_color_from_app_style.dart
@@ -4,60 +4,32 @@ import 'package:jvx_flutterclient/core/models/api/response/application_style_res
 import '../../models/api/response.dart';
 
 MaterialColor getColorFromAppStyle(ApplicationStyleResponse state) {
-  Map<int, Color> color = {
-    50: Color.fromRGBO(
-        state.themeColor.red,
-        state.themeColor.green,
-        state.themeColor.blue,
-        .1),
-    100: Color.fromRGBO(
-        state.themeColor.red,
-        state.themeColor.green,
-        state.themeColor.blue,
-        .2),
-    200: Color.fromRGBO(
-        state.themeColor.red,
-        state.themeColor.green,
-        state.themeColor.blue,
-        .3),
-    300: Color.fromRGBO(
-        state.themeColor.red,
-        state.themeColor.green,
-        state.themeColor.blue,
-        .4),
-    400: Color.fromRGBO(
-        state.themeColor.red,
-        state.themeColor.green,
-        state.themeColor.blue,
-        .5),
-    500: Color.fromRGBO(
-        state.themeColor.red,
-        state.themeColor.green,
-        state.themeColor.blue,
-        .6),
-    600: Color.fromRGBO(
-        state.themeColor.red,
-        state.themeColor.green,
-        state.themeColor.blue,
-        .7),
-    700: Color.fromRGBO(
-        state.themeColor.red,
-        state.themeColor.green,
-        state.themeColor.blue,
-        .8),
-    800: Color.fromRGBO(
-        state.themeColor.red,
-        state.themeColor.green,
-        state.themeColor.blue,
-        .9),
-    900: Color.fromRGBO(
-        state.themeColor.red,
-        state.themeColor.green,
-        state.themeColor.blue,
-        1),
-  };
+  if (state.themeColor != null) {
+    Map<int, Color> color = {
+      50: Color.fromRGBO(state.themeColor.red, state.themeColor.green,
+          state.themeColor.blue, .1),
+      100: Color.fromRGBO(state.themeColor.red, state.themeColor.green,
+          state.themeColor.blue, .2),
+      200: Color.fromRGBO(state.themeColor.red, state.themeColor.green,
+          state.themeColor.blue, .3),
+      300: Color.fromRGBO(state.themeColor.red, state.themeColor.green,
+          state.themeColor.blue, .4),
+      400: Color.fromRGBO(state.themeColor.red, state.themeColor.green,
+          state.themeColor.blue, .5),
+      500: Color.fromRGBO(state.themeColor.red, state.themeColor.green,
+          state.themeColor.blue, .6),
+      600: Color.fromRGBO(state.themeColor.red, state.themeColor.green,
+          state.themeColor.blue, .7),
+      700: Color.fromRGBO(state.themeColor.red, state.themeColor.green,
+          state.themeColor.blue, .8),
+      800: Color.fromRGBO(state.themeColor.red, state.themeColor.green,
+          state.themeColor.blue, .9),
+      900: Color.fromRGBO(state.themeColor.red, state.themeColor.green,
+          state.themeColor.blue, 1),
+    };
 
-  MaterialColor colorCustom =
-      MaterialColor(state.themeColor.value, color);
-  return colorCustom;
+    MaterialColor colorCustom = MaterialColor(state.themeColor.value, color);
+    return colorCustom;
+  }
+  return null;
 }
