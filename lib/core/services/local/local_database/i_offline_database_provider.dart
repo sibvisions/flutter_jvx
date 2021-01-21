e6613a4b55e19244ae456f0e5bbd627f8c05f47f
Jürgen Hörmann <j.hoermann77@gmail.com>
2021-01-21 11:46:25 +0100

    removed unused interface methods

--- lib/core/services/local/local_database/i_offline_database_provider.dart
+++ lib/core/services/local/local_database/i_offline_database_provider.dart
@@ -2,17 +2,10 @@ import 'package:flutter/cupertino.dart';
 
 import '../../../models/api/request.dart';
 import '../../../models/api/response.dart';
-import '../../../models/api/response/data/filter.dart';
 import 'i_database_provider.dart';
 
 abstract class IOfflineDatabaseProvider extends IDatabaseProvider {
   int syncProgress;
   Stream<Response> request(Request request);
   Future<bool> syncOnline(BuildContext context);
-  Future<bool> syncDelete(
-      BuildContext context, String dataProvider, Filter filter);
-  Future<bool> syncInsert(
-      BuildContext context, String dataProvider, List<dynamic> row);
-  Future<bool> syncUpdate(
-      BuildContext context, String dataProvider, List<dynamic> row);
 }
