cc05b09c98f86559c03d542469adee22eb6029d0
Lorenz Müksch <lorenz.mueksch@sibvisions.com>
2021-01-27 19:58:50 +0100

    another fix

--- lib/core/ui/widgets/page/open_screen_page_widget.dart
+++ lib/core/ui/widgets/page/open_screen_page_widget.dart
@@ -294,8 +294,10 @@ class _OpenScreenPageWidgetState extends State<OpenScreenPageWidget>
             leading: IconButton(
               icon: Icon(Icons.arrow_back),
               onPressed: () {
-                if (_openScreenManager
-                        .screens[currentIndex].configuration?.onBack !=
+                if (_openScreenManager.screens.values
+                        .toList()[currentIndex]
+                        .configuration
+                        ?.onBack !=
                     null) {
                   if (_openScreenManager.screens[currentIndex].configuration
                       .onBack()) {
