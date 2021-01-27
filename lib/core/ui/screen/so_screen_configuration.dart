76a92c17e1330a8c47e22fa04442537bfe4bcf73
Lorenz Müksch <lorenz.mueksch@sibvisions.com>
2021-01-27 14:27:44 +0100

    added offlinescreen parameter

--- lib/core/ui/screen/so_screen_configuration.dart
+++ lib/core/ui/screen/so_screen_configuration.dart
@@ -7,10 +7,12 @@ class SoScreenConfiguration extends ValueNotifier<Response> {
   final String screenTitle;
   final bool withServer;
   final String screenComponentId;
+  final bool offlineScreen;
 
   SoScreenConfiguration(Response response,
       {@required this.componentId,
       @required this.screenTitle,
+      this.offlineScreen,
       this.screenComponentId = "",
       this.withServer = true})
       : super(response);
