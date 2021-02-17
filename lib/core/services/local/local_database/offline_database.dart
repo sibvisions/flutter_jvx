3557ca806acda229d2d7b3a2cce9c3b2cbf71676
Jürgen Hörmann <j.hoermann77@gmail.com>
2021-02-17 10:10:07 +0100

    added applicationStyle request

--- lib/core/services/local/local_database/offline_database.dart
+++ lib/core/services/local/local_database/offline_database.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
+import 'package:jvx_flutterclient/core/models/api/request/application_style.dart';
 import 'package:jvx_flutterclient/core/models/api/request/logout.dart';
 
 import '../../../../injection_container.dart';
@@ -99,107 +100,124 @@ class OfflineDatabase extends LocalDatabase
       await for (Response response in bloc.startup(startup)) {
         if (response != null && !hasError(response)) {
           this._setProperties(bloc, response);
-          String currentScreenComponentId = "";
 
-          List<String> syncDataProvider = await this.getOfflineDataProvider();
-          Map<String, List<Map<String, dynamic>>> syncData =
-              Map<String, List<Map<String, dynamic>>>();
+          ApplicationStyle applicationStyle = ApplicationStyle(
+              clientId: bloc.appState.clientId,
+              requestType: RequestType.APP_STYLE,
+              name: 'applicationStyle',
+              contentMode: 'json');
 
-          // get sync data
-          await Future.forEach(syncDataProvider, (dataProvider) async {
-            if (dataProvider != null) {
-              syncData[dataProvider] = await this.getSyncData(dataProvider);
+          // application style request
+          await for (Response response
+              in bloc.applicationStyle(applicationStyle)) {
+            if (response != null && !hasError(response)) {
+              this._setProperties(bloc, response);
+              String currentScreenComponentId = "";
 
-              if (syncData[dataProvider] != null)
-                rowsToSync += syncData[dataProvider].length;
-            }
-          });
+              List<String> syncDataProvider =
+                  await this.getOfflineDataProvider();
+              Map<String, List<Map<String, dynamic>>> syncData =
+                  Map<String, List<Map<String, dynamic>>>();
 
-          // sync data to server
-          await Future.forEach(syncData.entries, (entry) async {
-            if (entry.value.length > 0) {
-              DataBookMetaData metaData = await getMetaDataBook(entry.key);
+              // get sync data
+              await Future.forEach(syncDataProvider, (dataProvider) async {
+                if (dataProvider != null) {
+                  syncData[dataProvider] = await this.getSyncData(dataProvider);
 
-              // open close screen
-              if (metaData.offlineScreenComponentId !=
-                  currentScreenComponentId) {
-                if (currentScreenComponentId.length > 0) {
-                  CloseScreen closeScreen = CloseScreen(
-                      componentId: currentScreenComponentId,
-                      clientId: bloc.appState.clientId,
-                      requestType: RequestType.CLOSE_SCREEN);
+                  if (syncData[dataProvider] != null)
+                    rowsToSync += syncData[dataProvider].length;
+                }
+              });
 
-                  await for (Response response
-                      in bloc.closeScreen(closeScreen)) {
-                    if (response != null && !hasError(response)) {
-                      currentScreenComponentId = "";
+              // sync data to server
+              await Future.forEach(syncData.entries, (entry) async {
+                if (entry.value.length > 0) {
+                  DataBookMetaData metaData = await getMetaDataBook(entry.key);
+
+                  // open close screen
+                  if (metaData.offlineScreenComponentId !=
+                      currentScreenComponentId) {
+                    if (currentScreenComponentId.length > 0) {
+                      CloseScreen closeScreen = CloseScreen(
+                          componentId: currentScreenComponentId,
+                          clientId: bloc.appState.clientId,
+                          requestType: RequestType.CLOSE_SCREEN);
+
+                      await for (Response response
+                          in bloc.closeScreen(closeScreen)) {
+                        if (response != null && !hasError(response)) {
+                          currentScreenComponentId = "";
+                        }
+                      }
+                    }
+
+                    SoAction action = SoAction(
+                        componentId: metaData.offlineScreenComponentId,
+                        label: "SyncOffline");
+                    OpenScreen openScreen = OpenScreen(
+                        action: action,
+                        clientId: bloc.appState.clientId,
+                        manualClose: false,
+                        requestType: RequestType.OPEN_SCREEN);
+                    await for (Response response
+                        in bloc.openScreen(openScreen)) {
+                      if (response != null && !hasError(response)) {
+                        currentScreenComponentId =
+                            metaData.offlineScreenComponentId;
+                      }
                     }
                   }
-                }
 
-                SoAction action = SoAction(
-                    componentId: metaData.offlineScreenComponentId,
-                    label: "SyncOffline");
-                OpenScreen openScreen = OpenScreen(
-                    action: action,
-                    clientId: bloc.appState.clientId,
-                    manualClose: false,
-                    requestType: RequestType.OPEN_SCREEN);
-                await for (Response response in bloc.openScreen(openScreen)) {
-                  if (response != null && !hasError(response)) {
-                    currentScreenComponentId =
-                        metaData.offlineScreenComponentId;
-                  }
+                  // sync insert, update, delete to server
+                  await Future.forEach(entry.value, (element) async {
+                    String state =
+                        OfflineDatabaseFormatter.getRowState(element);
+                    Map<String, dynamic> primaryKeyValues =
+                        OfflineDatabaseFormatter.getDataColumns(
+                            element, metaData.primaryKeyColumns);
+                    Filter primaryKeyFilter = Filter(
+                        columnNames: primaryKeyValues.keys.toList(),
+                        values: primaryKeyValues.values.toList());
+                    if (state == OFFLINE_ROW_STATE_DELETED) {
+                      if (await this.syncDelete(
+                          context,
+                          entry.key,
+                          primaryKeyFilter,
+                          metaData.columnNames,
+                          element)) rowsSynced++;
+                    } else if (state == OFFLINE_ROW_STATE_INSERTED) {
+                      if (await this.syncInsert(context, entry.key,
+                          primaryKeyFilter, metaData.columnNames, element)) {
+                        rowsSynced++;
+                      }
+                    } else if (state == OFFLINE_ROW_STATE_UPDATED) {
+                      if (await this.syncUpdate(context, entry.key,
+                          primaryKeyFilter, metaData.columnNames, element)) {
+                        rowsSynced++;
+                      }
+                    }
+                    setRowProgress(rowsToSync, rowsSynced);
+                  });
                 }
-              }
-
-              // sync insert, update, delete to server
-              await Future.forEach(entry.value, (element) async {
-                String state = OfflineDatabaseFormatter.getRowState(element);
-                Map<String, dynamic> primaryKeyValues =
-                    OfflineDatabaseFormatter.getDataColumns(
-                        element, metaData.primaryKeyColumns);
-                Filter primaryKeyFilter = Filter(
-                    columnNames: primaryKeyValues.keys.toList(),
-                    values: primaryKeyValues.values.toList());
-                if (state == OFFLINE_ROW_STATE_DELETED) {
-                  if (await this.syncDelete(
-                      context,
-                      entry.key,
-                      primaryKeyFilter,
-                      metaData.columnNames,
-                      element)) rowsSynced++;
-                } else if (state == OFFLINE_ROW_STATE_INSERTED) {
-                  if (await this.syncInsert(context, entry.key,
-                      primaryKeyFilter, metaData.columnNames, element)) {
-                    rowsSynced++;
-                  }
-                } else if (state == OFFLINE_ROW_STATE_UPDATED) {
-                  if (await this.syncUpdate(context, entry.key,
-                      primaryKeyFilter, metaData.columnNames, element)) {
-                    rowsSynced++;
-                  }
-                }
-                setRowProgress(rowsToSync, rowsSynced);
               });
-            }
-          });
 
-          // close screen if an screen is open
-          if (currentScreenComponentId.length > 0) {
-            CloseScreen closeScreen = CloseScreen(
-                componentId: currentScreenComponentId,
-                clientId: bloc.appState.clientId,
-                requestType: RequestType.CLOSE_SCREEN);
+              // close screen if an screen is open
+              if (currentScreenComponentId.length > 0) {
+                CloseScreen closeScreen = CloseScreen(
+                    componentId: currentScreenComponentId,
+                    clientId: bloc.appState.clientId,
+                    requestType: RequestType.CLOSE_SCREEN);
 
-            await for (Response response in bloc.closeScreen(closeScreen)) {
-              if (response != null && !hasError(response)) {
-                currentScreenComponentId = "";
+                await for (Response response in bloc.closeScreen(closeScreen)) {
+                  if (response != null && !hasError(response)) {
+                    currentScreenComponentId = "";
+                  }
+                }
               }
+
+              if (rowsSynced == rowsToSync) result = true;
             }
           }
-
-          if (rowsSynced == rowsToSync) result = true;
         }
       }
 
