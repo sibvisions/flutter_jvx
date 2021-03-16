385597d0b39127f799316fdb69992cd18d25fda7
Lorenz Müksch <lorenz.mueksch@sibvisions.com>
2021-03-16 15:08:48 +0100

    hopefully fix browser mobiel issue

--- lib/core/services/remote/remote.dart
+++ lib/core/services/remote/remote.dart
@@ -1,2 +1,1 @@
 export 'bloc/bloc.dart';
-export 'rest/rest.dart';
--- lib/core/services/remote/rest/rest.dart
+++
@@ -1,5 +0,0 @@
-export 'browser_client.dart';
-export 'http_client.dart';
-export 'http_client_stub.dart' hide getHttpClient;
-export 'mobile_client.dart' hide getHttpClient;
-export 'rest_client.dart';
