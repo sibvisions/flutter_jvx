81e6e62410a8e542526c882766ff00f839298bd7
Lorenz Müksch <lorenz.mueksch@sibvisions.com>
2021-03-16 14:19:10 +0100

    changed export files

--- lib/core/services/local/local.dart
+++ lib/core/services/local/local.dart
@@ -2,3 +2,7 @@ export 'custom/custom.dart';
 export 'local_database/local_database.dart';
 export 'local_database_manager.dart';
 export 'shared_preferences_manager.dart';
+export 'local_database/offline_database.dart' hide ProgressCallback;
+export 'local_database/offline_database_formatter.dart';
+export 'local_database/i_offline_database_provider.dart';
+export 'local_database/i_database_provider.dart';
