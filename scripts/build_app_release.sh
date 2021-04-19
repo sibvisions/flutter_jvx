1654fb3fd488987aef4ef239965d20fee02e9a87
Lorenz Müksch <lorenz.mueksch@sibvisions.com>
2021-04-19 13:48:04 +0200

    added build script

---
+++ scripts/build_app_release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+if [ $# -eq 1 ]
+  then
+    echo "Only 1 argument supplied"
+    exit 1
+fi
+
+if [ $# -eq 2 ]
+  then
+    echo "Building Flutter client"
+
+    echo "Version $1+$2"
+
+    flutter build apk --release --no-sound-null-safety --bundle-sksl-path flutter_01.sksl.json --build-name=$1 --build-number=$2
+fi
