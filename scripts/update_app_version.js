21db65d8c350c91ee8bd6db16ec5e82d22ca09ee
Jürgen Hörmann <j.hoermann77@gmail.com>
2021-09-29 16:07:30 +0200

    Removed unused build script

--- scripts/update_app_version.js
+++ scripts/update_app_version.js
@@ -36,112 +36,33 @@ fs
   })
   .catch((error) => console.warn(error));
 
-if (process.argv[2] != null) {
-  if (process.argv[2] == "build") {
-    setValue(path, (json) => {
-      const splittedVersion = json.version.split("+");
 
-      const updatedNumber = (parseInt(splittedVersion[1]) + 1).toString();
+var version;
+var commit;
 
-      splittedVersion[1] = updatedNumber;
+exec("git rev-parse --short HEAD", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+  }
 
-      json.version = splittedVersion.join("+");
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+  }
 
-      return json;
-    });
+  commit = stdout.trim();
 
-    setYamlValue(yamlPath, (yaml) => {
-      const splittedVersion = yaml.version.split("+");
+  getYamlValue(yamlPath, (yaml) => {
+    version = yaml.version;
+  });
 
-      const updatedNumber = (parseInt(splittedVersion[1]) + 1).toString();
+  setValue(path, (json) => {
+    var newDate = Date.now()
 
-      splittedVersion[1] = updatedNumber;
+    json.version = version;
+    json.date = newDate;
+    json.commit = commit;
 
-      yaml.version = splittedVersion.join("+");
+    return json;
+  });
+})
 
-      return yaml;
-    });
-
-
-  } else if (process.argv[2] == "commit") {
-    exec("git rev-parse --short HEAD", (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-      }
-
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-      }
-
-      setValue(path, (json) => {
-        json.commit = stdout.trim();
-        return json;
-      });
-    });
-  } else if (process.argv[2] == "date") {
-    setValue(path, (json) => {
-      var newDate = Date.now()
-
-      json.date = newDate;
-
-      return json;
-    });
-  } else if (process.argv[2] == "version") {
-
-    const newVersion = process.argv[3];
-
-    if (newVersion!=null) {
-      setValue(path, (json) => {
-        const splittedVersion = json.version.split("+");
-
-        splittedVersion[0] = newVersion;
-
-        json.version = splittedVersion.join("+");
-
-        return json;
-      });
-
-      setYamlValue(yamlPath, (yaml) => {
-        const splittedVersion = yaml.version.split("+");
-
-        splittedVersion[0] = newVersion;
-
-        yaml.version = splittedVersion.join("+");
-
-        return yaml;
-      });
-    } else {
-      console.log('Missing version!');
-    }  
-  } 
-} else {
-  var version;
-  var commit;
-
-  exec("git rev-parse --short HEAD", (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-    }
-
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-    }
-
-    commit = stdout.trim();
-    console.log('commit1' + commit);
-
-    getYamlValue(yamlPath, (yaml) => {
-      version = yaml.version;
-    });
-  
-    setValue(path, (json) => {
-      var newDate = Date.now()
-  
-      json.version = version;
-      json.date = newDate;
-      json.commit = commit;
-  
-      return json;
-    });
-  })
-}
