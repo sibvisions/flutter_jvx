c67877928a12d03e9c1c2cab03b63ed2df31813d
Michael-Schober <40370288+Michael-Schober@users.noreply.github.com>
2021-10-05 15:57:15 +0200

    FS#2569 fix null safety
    Fixes occuring null pointer, implement checking
    refactor updateProperties

--- lib/src/ui/component/model/component_model.dart
+++ lib/src/ui/component/model/component_model.dart
@@ -82,49 +82,117 @@ class ComponentModel with ChangeNotifier {
         lastLayout = changedComponent.created,
         appState = sl<AppState>();
 
+  // @mustCallSuper
+  // void updateProperties(
+  //     BuildContext context, ChangedComponent changedComponent) {
+  //   // preferredSize = changedComponent.getProperty<Size>(ComponentProperty.PREFERRED_SIZE, _preferredSize);
+  //   // maximumSize = changedComponent.getProperty<Size>(ComponentProperty.MAXIMUM_SIZE, _maximumSize);
+  //   // minimumSize = changedComponent.getProperty<Size>(ComponentProperty.MINIMUM_SIZE, _minimumSize);
+  //   // rawComponentId = changedComponent.getProperty<String>(ComponentProperty.ID, rawComponentId)!;
+  //   // background = changedComponent.getProperty<Color>(ComponentProperty.BACKGROUND, background) != null ? changedComponent.getProperty<Color>(ComponentProperty.BACKGROUND, background)! : Colors.white;
+  //   // name = changedComponent.getProperty<String>(ComponentProperty.NAME, name)!;
+  //   // isVisible = changedComponent.getProperty<bool>(ComponentProperty.VISIBLE, isVisible)!;
+  //   // fontStyle = SoTextStyle.addFontToTextStyle(changedComponent.getProperty<String>(ComponentProperty.FONT, null),fontStyle);
+  //
+  //   // textScaleFactor = appState.applicationStyle?.textScaleFactor ?? 1.0;
+  //
+  //   // foreground = changedComponent.getProperty<Color>(ComponentProperty.FOREGROUND, foreground)!;
+  //   // fontStyle = SoTextStyle.addForecolorToTextStyle(foreground, fontStyle)!;
+  //   // enabled = changedComponent.getProperty<bool>(ComponentProperty.ENABLED, enabled)!;
+  //   // verticalAlignment = changedComponent.getProperty<int>(ComponentProperty.VERTICAL_ALIGNMENT, verticalAlignment)!;
+  //   // horizontalAlignment = changedComponent.getProperty<int>(ComponentProperty.HORIZONTAL_ALIGNMENT, horizontalAlignment)!;
+  //   // parentComponentId = changedComponent.getProperty<String>(ComponentProperty.PARENT, parentComponentId)!;
+  //   // _constraints = changedComponent.getProperty<String>(ComponentProperty.CONSTRAINTS, constraints)!;
+  //   // name = changedComponent.getProperty<String>(ComponentProperty.NAME, name)!;
+  //   // text = changedComponent.getProperty<String>(ComponentProperty.TEXT, text)!;
+  //   // classNameEventSourceRef = _changedComponent.getProperty<String>(ComponentProperty.CLASS_NAME_EVENT_SOURCE_REF, classNameEventSourceRef);
+  //
+  //   toBeNamed(context, changedComponent);
+  // }
+
   @mustCallSuper
-  void updateProperties(
-      BuildContext context, ChangedComponent changedComponent) {
-    preferredSize = changedComponent.getProperty<Size>(
-        ComponentProperty.PREFERRED_SIZE, _preferredSize);
-    maximumSize = changedComponent.getProperty<Size>(
-        ComponentProperty.MAXIMUM_SIZE, _maximumSize);
-    minimumSize = changedComponent.getProperty<Size>(
-        ComponentProperty.MINIMUM_SIZE, _minimumSize);
-    rawComponentId = changedComponent.getProperty<String>(
-        ComponentProperty.ID, rawComponentId)!;
-    background = changedComponent.getProperty<Color>(
-                ComponentProperty.BACKGROUND, background) !=
-            null
-        ? changedComponent.getProperty<Color>(
-            ComponentProperty.BACKGROUND, background)!
-        : Colors.white;
-    name = changedComponent.getProperty<String>(ComponentProperty.NAME, name)!;
-    isVisible = changedComponent.getProperty<bool>(
-        ComponentProperty.VISIBLE, isVisible)!;
-    fontStyle = SoTextStyle.addFontToTextStyle(
-        changedComponent.getProperty<String>(ComponentProperty.FONT, null),
-        fontStyle);
+  void updateProperties(BuildContext context, ChangedComponent changedComponent){
+    // MetaData
+    //    RawComponentId
+    String? tempRawComponentId = changedComponent.getProperty<String>(ComponentProperty.ID, null);
+    if(tempRawComponentId != null)
+      rawComponentId = tempRawComponentId;
+    //    ParentId
+    String? tempParentComponentId = changedComponent.getProperty<String>(ComponentProperty.PARENT, null);
+    if(tempParentComponentId != null)
+      parentComponentId = tempParentComponentId;
+    //    Name
+    String? tempName = changedComponent.getProperty<String>(ComponentProperty.NAME, null);
+    if(tempName != null)
+      name = tempName;
+    //    Enabled
+    bool? tempEnabled = changedComponent.getProperty<bool>(ComponentProperty.ENABLED, null);
+    if(tempEnabled != null)
+      enabled = tempEnabled;
+    //    Is Visible
+    bool? tempIsVisible = changedComponent.getProperty<bool>(ComponentProperty.VISIBLE, null);
+    if(tempIsVisible != null)
+      isVisible = tempIsVisible;
+    //    ClassNameEventSourceRef
+    String? tempClassNameEventSourceRef = _changedComponent.getProperty<String>(ComponentProperty.CLASS_NAME_EVENT_SOURCE_REF, classNameEventSourceRef);
+    if(tempClassNameEventSourceRef != null)
+      classNameEventSourceRef = tempClassNameEventSourceRef;
 
+
+
+    // LayoutData
+    //    Preferred Size
+    Size? tempPreferredSize = changedComponent.getProperty<Size>(ComponentProperty.PREFERRED_SIZE, null);
+    if(tempPreferredSize != null)
+      preferredSize = tempPreferredSize;
+    //    Maximum Size
+    Size? tempMaximumSize = changedComponent.getProperty<Size>(ComponentProperty.MAXIMUM_SIZE, null);
+    if(tempMaximumSize != null)
+      maximumSize = tempMaximumSize;
+    //    MinimumSize
+    Size? tempMinimumSize = changedComponent.getProperty<Size>(ComponentProperty.MINIMUM_SIZE, null);
+    if(tempMinimumSize != null)
+      minimumSize = tempMinimumSize;
+    //    Constraints
+    String? tempConstraints = changedComponent.getProperty<String>(ComponentProperty.CONSTRAINTS, null);
+    if(tempConstraints != null)
+      _constraints = tempConstraints;
+    //    Horizontal Alignment
+    int? tempHorizontalAlignment = changedComponent.getProperty<int>(ComponentProperty.HORIZONTAL_ALIGNMENT, null);
+    if(tempHorizontalAlignment != null)
+      horizontalAlignment = tempHorizontalAlignment;
+    //    Vertical Alignment
+    int? tempVerticalAlignment = changedComponent.getProperty<int>(ComponentProperty.VERTICAL_ALIGNMENT, null);
+    if(tempVerticalAlignment != null)
+      verticalAlignment = tempVerticalAlignment;
+
+
+
+
+    // Appearance
+    //    TextScaleFactor
     textScaleFactor = appState.applicationStyle?.textScaleFactor ?? 1.0;
+    //    Text
+    String? tempText = changedComponent.getProperty<String>(ComponentProperty.TEXT, null);
+    if(tempText != null)
+      text = tempText;
+   //    Foreground Color
+    Color? tempForeground = changedComponent.getProperty<Color>(ComponentProperty.FOREGROUND, null);
+    if(tempForeground != null)
+      foreground = tempForeground;
+    //    Background Color
+    Color? tempBackground = changedComponent.getProperty<Color>(ComponentProperty.BACKGROUND, null);
+    if(tempBackground != null)
+      background = tempBackground;
 
-    foreground = changedComponent.getProperty<Color>(
-        ComponentProperty.FOREGROUND, foreground)!;
-    fontStyle = SoTextStyle.addForecolorToTextStyle(foreground, fontStyle)!;
-    enabled =
-        changedComponent.getProperty<bool>(ComponentProperty.ENABLED, enabled)!;
-    verticalAlignment = changedComponent.getProperty<int>(
-        ComponentProperty.VERTICAL_ALIGNMENT, verticalAlignment)!;
-    horizontalAlignment = changedComponent.getProperty<int>(
-        ComponentProperty.HORIZONTAL_ALIGNMENT, horizontalAlignment)!;
-    parentComponentId = changedComponent.getProperty<String>(
-        ComponentProperty.PARENT, parentComponentId)!;
-    _constraints = changedComponent.getProperty<String>(
-        ComponentProperty.CONSTRAINTS, constraints)!;
-    name = changedComponent.getProperty<String>(ComponentProperty.NAME, name)!;
-    text = changedComponent.getProperty<String>(ComponentProperty.TEXT, text)!;
-    classNameEventSourceRef = _changedComponent.getProperty<String>(
-        ComponentProperty.CLASS_NAME_EVENT_SOURCE_REF, classNameEventSourceRef);
+    //    Font Style
+    Color? tempTextForeground = changedComponent.getProperty<Color>(ComponentProperty.FOREGROUND, null);
+    String? tempFont = changedComponent.getProperty<String>(ComponentProperty.FONT, null);
+    if(tempTextForeground != null)
+      fontStyle = SoTextStyle.addForecolorToTextStyle(tempTextForeground, fontStyle);
+    if(tempFont != null)
+      fontStyle = SoTextStyle.addFontToTextStyle(tempFont, fontStyle);
+
 
     this.changedComponent = changedComponent;
     this.lastLayout = changedComponent.created;
