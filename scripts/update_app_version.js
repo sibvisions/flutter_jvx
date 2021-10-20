6b294e99ea78c1af8f4dbdb62881f584ebc7800a
Jürgen Hörmann <j.hoermann77@gmail.com>
2021-10-20 11:10:59 +0200

    Build script update

--- scripts/update_app_version.js
+++ scripts/update_app_version.js
@@ -1,7 +1,9 @@
 const { exec } = require("child_process");
 const fs = require("fs").promises;
+const YAML = require('yaml');
 
 const path = "assets/version/app_version.json";
+const yamlPath = "pubspec.yaml";
 
 const setValue = (fn, callback) =>
   fs
@@ -14,41 +16,65 @@ const setValue = (fn, callback) =>
     .then((body) => fs.writeFile(fn, body))
     .catch((error) => console.warn(error));
 
-if (process.argv[2] != null) {
-  if (process.argv[2] == "version") {
-    setValue(path, (json) => {
-      const splittedVersion = json.version.split("+");
+const getYamlValue = (fn, callback) =>
+fs
+  .readFile(fn, 'utf-8')
+  .then((body) => YAML.parse(body))
+  .then((yaml) => {
+    return callback(yaml);
+  })
+  .catch((error) => console.warn(error));
 
-      const updatedNumber = (parseInt(splittedVersion[1]) + 1).toString();
+const setYamlValue = (fn, callback) =>
+  fs
+    .readFile(fn, 'utf-8')
+    .then((body) => YAML.parse(body))
+    .then((yaml) => {
+      return callback(yaml);
+    })
+    .then((yaml) => YAML.stringify(yaml))
+    .then((body) => fs.writeFile(fn, body))
+    .catch((error) => console.warn(error));
 
-      splittedVersion[1] = updatedNumber;
+var version;
+var commit;
 
-      json.version = splittedVersion.join("+");
+if (process.argv[2] == null) {
+  console.log('Updating app version in: ' + path);
+  exec("git rev-parse --short HEAD", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+    }
 
-      return json;
-    });
-  } else if (process.argv[2] == "commit") {
-    exec("git rev-parse --short HEAD", (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-      }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+    }
 
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-      }
+    commit = stdout.trim();
+
+    getYamlValue(yamlPath, (yaml) => {
+      version = yaml.version;
 
       setValue(path, (json) => {
-        json.commit = stdout.trim();
+        var newDate = Date.now()
+    
+        json.version = version;
+        json.date = newDate;
+        json.commit = commit;
+    
         return json;
       });
     });
-  } else if (process.argv[2] == "date") {
-    setValue(path, (json) => {
-      var newDate = Date.now()
+  });
+} else if (process.argv[2] == "increaseBuildNo") {
+  setYamlValue(yamlPath, (yaml) => {
+    const splittedVersion = yaml.version.split("+");
+    const updatedNumber = (parseInt(splittedVersion[1]) + 1).toString();
+    splittedVersion[1] = updatedNumber;
 
-      json.date = newDate;
+    yaml.version = splittedVersion.join("+");
+    console.log('Increasing build no to version: ' + yaml.version);
 
-      return json;
-    });
-  }
+    return yaml;
+  });
 }
