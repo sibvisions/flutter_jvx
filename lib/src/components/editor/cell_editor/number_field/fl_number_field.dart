efacd03edc2171915300d081904bb95bd2baf7ad
Toni Heiss <toni.heiss@sibvisions.com>
2022-02-15 16:27:03 +0100

    delete

--- lib/src/components/editor/cell_editor/number_field/fl_number_field.dart
+++ lib/src/components/editor/cell_editor/number_field/fl_number_field.dart
@@ -1,35 +1,39 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// import 'package:flutter_client/src/components/editor/text_field/fl_text_field_widget.dart';
-// import 'package:flutter_client/src/model/component/editor/fl_number_field_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_client/src/components/editor/cell_editor/number_field/numeric_text_formatter.dart';
+import 'package:flutter_client/src/components/editor/text_field/fl_text_field_widget.dart';
+import 'package:flutter_client/src/model/component/editor/fl_text_field_model.dart';
 
-// class FlNumberFieldWidget extends FlTextFieldWidget<FlNumberFieldModel> {
-//   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//   // Initialization
-//   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class FlNumberFieldWidget extends FlTextFieldWidget {
+  final NumericTextFormatter numberFormatter;
 
-//   const FlNumberFieldWidget(
-//       {Key? key,
-//       required FlNumberFieldModel model,
-//       required Function(String) valueChanged,
-//       required Function(String) endEditing,
-//       required FocusNode focusNode,
-//       required TextEditingController textController})
-//       : super(
-//             key: key,
-//             model: model,
-//             valueChanged: valueChanged,
-//             endEditing: endEditing,
-//             focusNode: focusNode,
-//             textController: textController);
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Initialization
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-//   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//   // Overridden widget defaults
-//   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  const FlNumberFieldWidget(
+      {Key? key,
+      required FlTextFieldModel model,
+      required Function(String) valueChanged,
+      required Function(String) endEditing,
+      required FocusNode focusNode,
+      required TextEditingController textController,
+      required this.numberFormatter})
+      : super(
+            key: key,
+            model: model,
+            valueChanged: valueChanged,
+            endEditing: endEditing,
+            focusNode: focusNode,
+            textController: textController);
 
-//   @override
-//   TextInputType get keyboardType => const TextInputType.numberWithOptions(signed: true, decimal: true);
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Overridden widget defaults
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-//   @override
-//   List<TextInputFormatter>? get inputFormatters => [NumericTextFormatter];
-// }
+  @override
+  TextInputType get keyboardType => const TextInputType.numberWithOptions(signed: true, decimal: true);
+
+  @override
+  List<TextInputFormatter>? get inputFormatters => [numberFormatter];
+}
