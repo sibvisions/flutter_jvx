0176fb075d9780ff29f80a38cec86cc6d8bfa684
Toni Heiss <toni.heiss@sibvisions.com>
2022-02-21 16:33:12 +0100

    Split panel

--- lib/src/components/split_panel/fl_split_panel_widget.dart
+++ lib/src/components/split_panel/fl_split_panel_widget.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_client/src/components/base_wrapper/base_comp_wrapper_widget.dart';
+import 'package:flutter_client/src/components/base_wrapper/fl_stateless_widget.dart';
+import 'package:flutter_client/src/mixin/ui_service_mixin.dart';
+import 'package:flutter_client/src/model/component/panel/fl_split_panel_model.dart';
+import 'package:flutter_client/src/model/layout/layout_data.dart';
+import 'package:flutter_client/util/extensions/list_extensions.dart';
 
-class FlSplitPanelWidget extends StatelessWidget {
-  const FlSplitPanelWidget({Key? key, required this.children}) : super(key: key);
+class FlSplitPanelWidget extends FlStatelessWidget<FlSplitPanelModel> with UiServiceMixin {
+  FlSplitPanelWidget({Key? key, required FlSplitPanelModel model, required this.children})
+      : super(key: key, model: model);
 
   final List<Widget> children;
-
 
   @override
   Widget build(BuildContext context) {
--- lib/src/components/split_panel/fl_split_panel_wrapper.dart
+++ lib/src/components/split_panel/fl_split_panel_wrapper.dart
@@ -1,8 +1,14 @@
+import 'dart:math';
+import 'dart:developer' as dev;
+
 import 'package:flutter/material.dart';
+import 'package:flutter_client/src/layout/split_layout.dart';
+import 'package:flutter_client/src/model/layout/layout_position.dart';
+import 'package:flutter_client/util/constants/i_color.dart';
 import '../base_wrapper/base_comp_wrapper_widget.dart';
 import '../base_wrapper/base_cont_wrapper_state.dart';
 import 'fl_split_panel_widget.dart';
-import '../../model/component/panel/fl_split_panel.dart';
+import '../../model/component/panel/fl_split_panel_model.dart';
 
 class FlSplitPanelWrapper extends BaseCompWrapperWidget<FlSplitPanelModel> {
   const FlSplitPanelWrapper({Key? key, required FlSplitPanelModel model}) : super(key: key, model: model);
@@ -12,15 +18,125 @@ class FlSplitPanelWrapper extends BaseCompWrapperWidget<FlSplitPanelModel> {
 }
 
 class _FlSplitPanelWrapperState extends BaseContWrapperState<FlSplitPanelModel> {
+  bool canSendAgain = true;
+
   @override
   void initState() {
     super.initState();
+
+    layoutData.layout = SplitLayout(splitAlignment: model.orientation, leftTopRatio: model.dividerPosition);
+
+    buildChildren();
+    registerParent();
+  }
+
+  @override
+  receiveNewModel({required FlSplitPanelModel newModel}) {
+    super.receiveNewModel(newModel: newModel);
+
+    layoutData.layout = SplitLayout(splitAlignment: newModel.orientation, leftTopRatio: newModel.dividerPosition);
+
+    buildChildren();
+    registerParent();
   }
 
   @override
   Widget build(BuildContext context) {
-    var panelWidget = FlSplitPanelWidget(children: children.values.toList());
+    var panelWidget = FlSplitPanelWidget(model: model, children: [...children.values.toList(), getDragSlider()]);
 
     return getPositioned(child: panelWidget);
+  }
+
+  Widget getDragSlider() {
+    if (layoutData.hasPosition) {
+      SplitLayout splitLayout = (layoutData.layout as SplitLayout);
+      LayoutPosition currentPosition = layoutData.layoutPosition!;
+
+      dev.log(currentPosition.toString());
+
+      double top = 0.0;
+      double left = 0;
+
+      double width = splitLayout.splitterSize;
+      double height = splitLayout.splitterSize;
+
+      if (model.orientation == SPLIT_ORIENTATION.HORIZONTAL) {
+        width = currentPosition.width;
+        top = (currentPosition.height * (splitLayout.leftTopRatio / 100.0)) - (splitLayout.splitterSize / 2);
+      } else {
+        height = currentPosition.height;
+        left = (currentPosition.width * (splitLayout.leftTopRatio / 100.0)) - (splitLayout.splitterSize / 2);
+      }
+
+      dev.log("$top, $left | $width, $height");
+      return Positioned(
+        top: top,
+        left: left,
+        width: width,
+        height: height,
+        child: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onVerticalDragUpdate: model.orientation == SPLIT_ORIENTATION.HORIZONTAL ? _verticalDrag : null,
+          onVerticalDragEnd: model.orientation == SPLIT_ORIENTATION.HORIZONTAL ? _verticalDragEnd : null,
+          onHorizontalDragUpdate: model.orientation == SPLIT_ORIENTATION.VERTICAL ? _horizontalDrag : null,
+          onHorizontalDragEnd: model.orientation == SPLIT_ORIENTATION.VERTICAL ? _horizontalDragEnd : null,
+          child: Container(
+            color: IColorConstants.COMPONENT_DISABLED,
+            // child: Center(
+            //     child: ClipRRect(
+            //         borderRadius: BorderRadius.circular(5.0),
+            //         child: Container(
+            //           height: 4,
+            //           color: IColorConstants.COMPONENT_DISABLED,
+            //           width: 40,
+            //         )))),
+          ),
+        ),
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  void _verticalDragEnd(DragEndDetails pDragDetails) {
+    registerParent();
+  }
+
+  void _verticalDrag(DragUpdateDetails pDragDetails) {
+    final RenderBox container = context.findRenderObject() as RenderBox;
+    final pos = container.globalToLocal(pDragDetails.globalPosition);
+
+    SplitLayout splitLayout = (layoutData.layout as SplitLayout);
+    splitLayout.leftTopRatio = min(1.0, pos.dy / container.size.height) * 100;
+
+    if (canSendAgain) {
+      canSendAgain = false;
+      registerParent();
+
+      Future.delayed(SplitLayout.UPDATE_INTERVALL, () {
+        canSendAgain = true;
+      });
+    }
+  }
+
+  void _horizontalDragEnd(DragEndDetails pDragDetails) {
+    registerParent();
+  }
+
+  void _horizontalDrag(DragUpdateDetails pDragDetails) {
+    final RenderBox container = context.findRenderObject() as RenderBox;
+    final pos = container.globalToLocal(pDragDetails.globalPosition);
+
+    SplitLayout splitLayout = (layoutData.layout as SplitLayout);
+    splitLayout.leftTopRatio = min(1.0, pos.dx / container.size.width) * 100;
+
+    if (canSendAgain) {
+      canSendAgain = false;
+      registerParent();
+
+      Future.delayed(SplitLayout.UPDATE_INTERVALL, () {
+        canSendAgain = true;
+      });
+    }
   }
 }
