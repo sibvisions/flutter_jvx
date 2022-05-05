570c234b686f4ab4139414244dc234c2279580df
Michael-Schober <40370288+Michael-Schober@users.noreply.github.com>
2022-05-05 17:22:19 +0200

    startup upon missing client id

--- lib/src/service/command/shared/processor/api/login_command_processor.dart
+++ lib/src/service/command/shared/processor/api/login_command_processor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_client/src/model/api/requests/api_login_request.dart';
+import 'package:flutter_client/src/model/command/api/startup_command.dart';
 
 import '../../../../../mixin/api_service_mixin.dart';
 import '../../../../../mixin/config_service_mixin.dart';
@@ -8,7 +9,7 @@ import '../../i_command_processor.dart';
 
 class LoginCommandProcessor with ApiServiceMixin, ConfigServiceMixin implements ICommandProcessor<LoginCommand> {
   @override
-  Future<List<BaseCommand>> processCommand(LoginCommand command) {
+  Future<List<BaseCommand>> processCommand(LoginCommand command) async {
     String? clientId = configService.getClientId();
 
     if (clientId != null) {
@@ -22,7 +23,7 @@ class LoginCommandProcessor with ApiServiceMixin, ConfigServiceMixin implements 
       );
       return apiService.sendRequest(request: loginRequest);
     } else {
-      throw Exception("NO ClIENT ID FOUND, while trying to send login Request");
+      return [StartupCommand(reason: "Login failed")];
     }
   }
 }
