7a7fbde2d8bc7179dcd44cb1e54b06b5b7ad82aa
Michael-Schober <40370288+Michael-Schober@users.noreply.github.com>
2022-06-01 16:26:58 +0200

    filter fix

--- lib/src/model/api/requests/api_filter_model.dart
+++ lib/src/model/api/requests/api_filter_model.dart
@@ -1,60 +1,22 @@
 import 'package:flutter_client/src/model/api/api_object_property.dart';
 
-class CompareType {
-  static const String EQUALS = "equals";
-  static const String LIKE = "like";
-  static const String LIKE_IGNORE_CASE = "LikeIgnoreCase";
-  static const String LIKE_REVERSE = "LikeReverse";
-  static const String LIKE_REVERSE_IGNORE_CASE = "LikeReverseIgnoreCase";
-  static const String LESS = "Less";
-  static const String LESS_EQUALS = "LessEquals";
-  static const String GREATER = "Greater";
-  static const String GREATER_EQUALS = "GreaterEquals";
-  static const String CONTAINS_IGNORE_CASE = "ContainsIgnoreCase";
-  static const String STARTS_WITH_IGNORE_CASE = "StartsWithIgnoreCase";
-  static const String ENDS_WITH_IGNORE_CASE = "EndsWithIgnoreCase";
-}
-
-class OperatorType {
-  static const String OR = "Or";
-  static const String AND = "And";
-}
-
 class ApiFilterModel {
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Class members
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   /// Name of the column from the value
-  final String columnName;
+  final List<String> columnNames;
 
-  /// Value used for filtering
-  final dynamic value;
-
-  /// See [CompareType] class for all available
-  final String? compareType;
-
-  /// See [OperatorType] class for all available
-  final String? operatorType;
-
-  final bool not;
-
-  ApiFilterModel? condition;
-
-  List<ApiFilterModel>? conditions;
+  final List<dynamic> values;
 
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Initialization
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   ApiFilterModel({
-    required this.columnName,
-    required this.value,
-    required this.compareType,
-    required this.operatorType,
-    this.not = false,
-    this.condition,
-    this.conditions,
+    required this.values,
+    required this.columnNames,
   });
 
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -62,12 +24,7 @@ class ApiFilterModel {
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   Map<String, dynamic> toJson() => {
-        ApiObjectProperty.columnName: columnName,
-        ApiObjectProperty.value: value,
-        ApiObjectProperty.compareType: compareType,
-        ApiObjectProperty.operatorType: operatorType,
-        ApiObjectProperty.not: not,
-        ApiObjectProperty.condition: condition,
-        ApiObjectProperty.conditions: conditions,
+        ApiObjectProperty.columnNames: columnNames,
+        ApiObjectProperty.values: values,
       };
 }
