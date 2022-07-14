7592510d9ea69584c063c9fa39835690522fb14f
Toni Heiss <toni.heiss@sibvisions.com>
2022-07-14 11:36:05 +0200

    fix init app multiple calls

--- lib/src/mask/splash/splash_widget.dart
+++ lib/src/mask/splash/splash_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter_client/src/mask/splash/loading_widget.dart';
 
 import '../../../init_app_mobile.dart' if (dart.library.html) '../../../init_app_web.dart';
 
-class SplashWidget extends StatelessWidget {
+class SplashWidget extends StatefulWidget {
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Class members
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -22,14 +22,36 @@ class SplashWidget extends StatelessWidget {
     this.languageCallbacks,
   }) : super(key: key);
 
+  @override
+  State<SplashWidget> createState() => _SplashWidgetState();
+}
+
+class _SplashWidgetState extends State<SplashWidget> {
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Class members
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  late Future<bool> initAppFuture;
+
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Overridden methods
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   @override
+  void initState() {
+    super.initState();
+
+    initAppFuture = initApp(
+      initContext: context,
+      languageCallbacks: widget.languageCallbacks,
+      styleCallbacks: widget.styleCallbacks,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: initApp(initContext: context, languageCallbacks: languageCallbacks, styleCallbacks: styleCallbacks),
+      future: initAppFuture,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         return const LoadingWidget();
       },
