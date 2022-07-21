8f357fc00018879c613429c754857e0a61ad3a24
Toni Heiss <toni.heiss@sibvisions.com>
2022-07-21 14:57:32 +0200

    Update api_isolate_get_repository_message.dart

--- lib/src/service/api/impl/isolate/messages/api_isolate_get_repository_message.dart
+++ lib/src/service/api/impl/isolate/messages/api_isolate_get_repository_message.dart
@@ -1,6 +1,5 @@
-import '../../../shared/i_repository.dart';
 import '../../../../isolate/isolate_message.dart';
-import '../../../../../model/command/base_command.dart';
+import '../../../shared/i_repository.dart';
 
 /// Used to send [IRepository] to the APIs isolate to be executed
 class ApiIsolateGetRepositoryMessage extends IsolateMessage<IRepository> {
