a4593c823effed27f92e1e5be8a342d8fc3ba03b
Bungeefan <severin.hamader@yahoo.de>
2022-07-22 11:32:41 +0200

    Added return values

--- lib/src/service/api/shared/repository/offline/offline_database.dart
+++ lib/src/service/api/shared/repository/offline/offline_database.dart
@@ -212,7 +212,7 @@ CREATE TABLE IF NOT EXISTS $OFFLINE_METADATA_TABLE (
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   /// Returns true if table with [pTableName] exists
-  tableExists(String pTableName) {
+  Future<bool> tableExists(String pTableName) {
     String sql = 'SELECT COUNT(*) AS COUNT FROM sqlite_master WHERE TYPE = \'table\' AND NAME = ?;';
     return db.rawQuery(sql, [formatOfflineTableName(pTableName)])
         // Check if returned count in select is 1
@@ -237,7 +237,7 @@ CREATE TABLE IF NOT EXISTS $OFFLINE_METADATA_TABLE (
   /// Checks if any row exists in [pTableName]
   /// if [pWhere] is not null, check with [pWhere] applied.
   /// Returns true if at least one row is returned.
-  rowExists({required String pTableName, Map<String, dynamic>? pFilter}) {
+  Future<bool> rowExists({required String pTableName, Map<String, dynamic>? pFilter}) {
     String sql = 'SELECT COUNT(*) AS COUNT FROM "${formatOfflineTableName(pTableName)}"';
     if (pFilter != null) {
       sql += _buildWhere(pFilter.keys);
@@ -260,18 +260,19 @@ CREATE TABLE IF NOT EXISTS $OFFLINE_METADATA_TABLE (
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   /// Executes a INSERT query, sets the state to Inserting and returns the last inserted row ID.
-  insert({required String pTableName, required Map<String, dynamic> pInsert, Transaction? txn}) {
+  Future<int> insert({required String pTableName, required Map<String, dynamic> pInsert, Transaction? txn}) {
     pInsert['"$STATE_COLUMN"'] = ROW_STATE_INSERTED;
     return rawInsert(pTableName: pTableName, pInsert: pInsert, txn: txn);
   }
 
   /// Executes a SQL INSERT query and returns the last inserted row ID.
-  rawInsert({required String pTableName, required Map<String, dynamic> pInsert, Transaction? txn}) {
+  Future<int> rawInsert({required String pTableName, required Map<String, dynamic> pInsert, Transaction? txn}) {
     return (txn ?? db).insert(formatOfflineTableName(pTableName), pInsert);
   }
 
   /// Executes a SQL UPDATE query and returns the number of changes made.
-  update({required String pTableName, required Map<String, dynamic> pUpdate, Map<String, dynamic>? pFilter}) {
+  Future<int> update(
+      {required String pTableName, required Map<String, dynamic> pUpdate, Map<String, dynamic>? pFilter}) {
     //TODO test solution
     var where = _getWhere(pFilter);
     var offlineTableName = formatOfflineTableName(pTableName);
@@ -299,7 +300,7 @@ CREATE TABLE IF NOT EXISTS $OFFLINE_METADATA_TABLE (
   }
 
   /// Executes a SQL DELETE query and returns the number of changes made.
-  delete({required String pTableName, Map<String, dynamic>? pFilter}) {
+  Future<int> delete({required String pTableName, Map<String, dynamic>? pFilter}) {
     var where = _getWhere(pFilter);
     //Just set state to Deleted (D)
     return db.update(formatOfflineTableName(pTableName), {'"$STATE_COLUMN"': ROW_STATE_DELETED},
