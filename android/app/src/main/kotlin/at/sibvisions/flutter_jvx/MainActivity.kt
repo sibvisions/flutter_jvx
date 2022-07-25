9e0c543ac3dd1455c436c6590ecddc911edc4c68
Bungeefan <severin.hamader@yahoo.de>
2022-07-25 17:24:25 +0200

    Added App Icons and Splash Screen for Android

--- android/app/src/main/kotlin/at/sibvisions/flutter_jvx/MainActivity.kt
+++ android/app/src/main/kotlin/at/sibvisions/flutter_jvx/MainActivity.kt
@@ -1,6 +1,14 @@
 package at.sibvisions.flutter_jvx
 
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+    }
 }
