827e12de539cb6852b67674b51e519135b44e045
Bungeefan <severin.hamader@yahoo.de>
2022-07-25 17:55:56 +0200

    Added check for Splash Screen in Android 12 and above

--- android/app/src/main/kotlin/at/sibvisions/flutter_jvx/MainActivity.kt
+++ android/app/src/main/kotlin/at/sibvisions/flutter_jvx/MainActivity.kt
@@ -1,5 +1,6 @@
 package at.sibvisions.flutter_jvx
 
+import android.os.Build
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
@@ -7,8 +8,10 @@ import io.flutter.embedding.android.FlutterActivity
 class MainActivity : FlutterActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Handle the splash screen transition.
-        val splashScreen = installSplashScreen()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // Handle the splash screen transition.
+            val splashScreen = installSplashScreen()
+        }
         super.onCreate(savedInstanceState)
     }
 }
