28a3c19c98ee81177fe449c60edda535c6c64e2b
Bungeefan <severin.hamader@yahoo.de>
2022-08-04 19:35:43 +0200

    Formatted

--- lib/src/mask/camera/qr_parser.dart
+++ lib/src/mask/camera/qr_parser.dart
@@ -1,30 +1,27 @@
 import 'dart:convert';
 
 class QRParser {
-
   static QRAppCode parseCode({required String rawQRCode}) {
-
     // Static
-    const Map<String,String> propertyNameMap = {
-      "Applikation" : "APPNAME",
-      "Application" : "APPNAME",
-      "PWD" : "PWD",
-      "USER" : "USER",
-      "URL" : "URL"
+    const Map<String, String> propertyNameMap = {
+      "Applikation": "APPNAME",
+      "Application": "APPNAME",
+      "PWD": "PWD",
+      "USER": "USER",
+      "URL": "URL"
     };
 
     Map<String, String> json = {};
 
-
     // If QR-Code is a json it can be easily parsed, otherwise string is split
     // by newLines.
-    try{
+    try {
       json = jsonDecode(rawQRCode);
     } on Exception {
       LineSplitter ls = const LineSplitter();
       List<String> properties = ls.convert(rawQRCode);
 
-      for(String prop in properties) {
+      for (String prop in properties) {
         List<String> splitProp = prop.split(": ");
         String propertyName = propertyNameMap[splitProp[0]]!;
         String propertyValue = splitProp[1];
@@ -34,23 +31,23 @@ class QRParser {
 
     return QRAppCode.fromJson(pJson: json);
   }
-
 }
-
 
 /// All possible info of an APP-QR Code
 class QRAppCode {
-
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Class members
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   /// Url of the remote server
   final String url;
+
   /// Name of the app
   final String appName;
+
   /// Username for auto-login
   final String? username;
+
   /// Password for auto-login
   final String? password;
 
@@ -58,17 +55,11 @@ class QRAppCode {
   // Initialization
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  QRAppCode({
-    required this.appName,
-    required this.url,
-    this.password,
-    this.username
-  });
+  QRAppCode({required this.appName, required this.url, this.password, this.username});
 
-  QRAppCode.fromJson({required Map<String, String> pJson}) :
-    username = pJson["USER"],
-    password = pJson["PWD"],
-    appName = pJson["APPNAME"]!,
-    url = pJson["URL"]!;
-
+  QRAppCode.fromJson({required Map<String, String> pJson})
+      : username = pJson["USER"],
+        password = pJson["PWD"],
+        appName = pJson["APPNAME"]!,
+        url = pJson["URL"]!;
 }
