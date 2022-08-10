2eecbddbcae9c2299dcc2da9282f7b2e65343e87
Toni Heiss <toni.heiss@sibvisions.com>
2022-08-10 11:35:37 +0200

    export col def

--- lib/data.dart
+++ lib/data.dart
@@ -1,3 +1,4 @@
+export 'src/model/data/column_definition.dart';
 export 'src/model/data/data_book.dart';
 export 'src/model/data/subscriptions/data_chunk.dart';
 export 'src/model/data/subscriptions/data_record.dart';
