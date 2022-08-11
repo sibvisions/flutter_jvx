b6e92609d5e8f6f3b47fb23037bec130f957c2b0
Bungeefan <severin.hamader@yahoo.de>
2022-08-11 11:19:15 +0200

    Export DalMetaDataResponse

--- lib/data.dart
+++ lib/data.dart
@@ -5,3 +5,4 @@ export 'src/model/data/subscriptions/data_chunk.dart';
 export 'src/model/data/subscriptions/data_record.dart';
 export 'src/model/data/subscriptions/data_subscription.dart';
 export 'src/model/request/filter.dart';
+export 'src/model/response/dal_meta_data_response.dart';
