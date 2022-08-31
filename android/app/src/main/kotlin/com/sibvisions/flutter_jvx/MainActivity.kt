4a7d3063f20b3dadc2b24f1906273695d45c89a2
Bungeefan <severin.hamader@yahoo.de>
2022-08-31 13:16:20 +0200

    Disable the Android splash screen fade out animation to avoid a flicker before the similar frame is drawn in Flutter

--- android/app/src/main/kotlin/com/sibvisions/flutter_jvx/MainActivity.kt
+++ android/app/src/main/kotlin/com/sibvisions/flutter_jvx/MainActivity.kt
@@ -11,6 +11,10 @@ class MainActivity : FlutterActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             // Handle the splash screen transition.
             val splashScreen = installSplashScreen()
+
+            // Disable the Android splash screen fade out animation to avoid
+            // a flicker before the similar frame is drawn in Flutter.
+            splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
         }
         super.onCreate(savedInstanceState)
     }
