ea2d33a83a92399dc2613d4bbea4a7f9033803c2
Bungeefan <severin.hamader@yahoo.de>
2022-09-12 17:27:06 +0200

    Fixed text color

--- lib/src/mask/menu/list/widget/app_menu_list_item.dart
+++ lib/src/mask/menu/list/widget/app_menu_list_item.dart
@@ -46,6 +46,7 @@ class AppMenuListItem extends StatelessWidget with ConfigServiceGetterMixin, UiS
       title: Text(
         menuItemModel.label,
         overflow: TextOverflow.ellipsis,
+        style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
       ),
       onTap: () =>
           onClick(pScreenLongName: menuItemModel.screenLongName, pUiService: getUiService(), pContext: context),
