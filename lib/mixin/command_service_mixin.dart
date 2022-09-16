748b641d0b77b288bf37321f6e78595724ea0e0e
Bungeefan <severin.hamader@yahoo.de>
2022-09-16 11:05:30 +0200

    Added interface exports

--- lib/mixin/command_service_mixin.dart
+++ lib/mixin/command_service_mixin.dart
@@ -1,10 +1,12 @@
 import '../src/service/command/i_command_service.dart';
 import '../src/service/service.dart';
 
+export '../src/service/command/i_command_service.dart';
+export '../src/service/command/impl/command_service.dart';
+
 ///
 /// Provides an [ICommandService] instance from get.it service
 ///
-
 mixin CommandServiceGetterMixin {
   ICommandService getCommandService() {
     return services<ICommandService>();
