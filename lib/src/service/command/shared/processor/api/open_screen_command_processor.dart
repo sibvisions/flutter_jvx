f9732df5e7a5cbc19ae2eaa449a19c02536f59b4
Bungeefan <severin.hamader@yahoo.de>
2022-10-06 18:16:28 +0200

    Removed isOpenScreen and secondRun logic

--- lib/src/service/command/shared/processor/api/open_screen_command_processor.dart
+++ lib/src/service/command/shared/processor/api/open_screen_command_processor.dart
@@ -3,13 +3,10 @@ import '../../../../../model/command/base_command.dart';
 import '../../../../../model/request/api_open_screen_request.dart';
 import '../../../../api/i_api_service.dart';
 import '../../i_command_processor.dart';
-import '../ui/update_components_command_processor.dart';
 
 class OpenScreenCommandProcessor implements ICommandProcessor<OpenScreenCommand> {
   @override
-  Future<List<BaseCommand>> processCommand(OpenScreenCommand command) async {
-    UpdateComponentsCommandProcessor.isOpenScreen = true;
-
+  Future<List<BaseCommand>> processCommand(OpenScreenCommand command) {
     return IApiService().sendRequest(
       request: ApiOpenScreenRequest(
         screenLongName: command.screenLongName,
--- lib/src/service/command/shared/processor/ui/update_components_command_processor.dart
+++ lib/src/service/command/shared/processor/ui/update_components_command_processor.dart
@@ -7,55 +7,44 @@ import '../../../../../model/command/ui/update_components_command.dart';
 import '../../i_command_processor.dart';
 
 class UpdateComponentsCommandProcessor implements ICommandProcessor<UpdateComponentsCommand> {
-  static bool isOpenScreen = false;
-  static bool _secondRun = false;
-
   @override
   Future<List<BaseCommand>> processCommand(UpdateComponentsCommand command) async {
-    FlutterJVx.log.d("------------------- Components are updating");
+    final stopwatch = Stopwatch()..start();
+    await ILayoutService().setValid(isValid: false);
 
-    if (!isOpenScreen && !_secondRun) {
-      await ILayoutService().setValid(isValid: false);
-    }
-
-    // Check to see if layout is currently busy
-    Future isLegal = Future.doWhile(() async {
-      bool isBusy = await ILayoutService().layoutInProcess();
+    // Wait as long as layout is busy
+    bool isBusy = true;
+    while (isBusy) {
+      isBusy = await ILayoutService().layoutInProcess();
 
       if (isBusy) {
         await Future.delayed(const Duration(milliseconds: 10));
       }
-
-      return isBusy;
-    });
+    }
 
     // Update components when current layout run is finished
-    await isLegal.then((_) async {
-      if (!isOpenScreen && !_secondRun) {
-        await ILayoutService().setValid(isValid: true);
+    await ILayoutService().setValid(isValid: true);
+    stopwatch.stop();
 
-        _secondRun = isOpenScreen;
-        isOpenScreen = false;
-      }
+    if (stopwatch.elapsedMilliseconds > 50) {
+      FlutterJVx.log.w("Layout was busy for ${stopwatch.elapsedMilliseconds}ms");
+    }
 
-      List<Future> futureList = [];
-      futureList.addAll(command.affectedComponents.map((e) => ILayoutService().markLayoutAsDirty(pComponentId: e)));
-      futureList.addAll(command.changedComponents.map((e) => ILayoutService().markLayoutAsDirty(pComponentId: e.id)));
-      futureList.addAll(command.deletedComponents.map((e) => ILayoutService().removeLayout(pComponentId: e)));
+    List<Future> futureList = [];
+    futureList.addAll(command.affectedComponents.map((e) => ILayoutService().markLayoutAsDirty(pComponentId: e)));
+    futureList.addAll(command.changedComponents.map((e) => ILayoutService().markLayoutAsDirty(pComponentId: e.id)));
+    futureList.addAll(command.deletedComponents.map((e) => ILayoutService().removeLayout(pComponentId: e)));
 
-      // Update Components in UI after all are marked as dirty
-      await Future.wait(futureList).then((value) {
-        IUiService().deleteInactiveComponent(inactiveIds: command.deletedComponents);
+    // Update Components in UI after all are marked as dirty
+    await Future.wait(futureList).then((value) {
+      IUiService().deleteInactiveComponent(inactiveIds: command.deletedComponents);
 
-        IUiService().saveNewComponents(newModels: command.newComponents.reversed.toList());
+      IUiService().saveNewComponents(newModels: command.newComponents.reversed.toList());
 
-        // List is reversed as to update all children before their respective parents.
-        IUiService().notifyChangedComponents(updatedModels: command.changedComponents.reversed.toList());
+      // List is reversed as to update all children before their respective parents.
+      IUiService().notifyChangedComponents(updatedModels: command.changedComponents.reversed.toList());
 
-        IUiService().notifyAffectedComponents(affectedIds: command.affectedComponents);
-
-        FlutterJVx.log.d("------------------- Components are finished updating");
-      });
+      IUiService().notifyAffectedComponents(affectedIds: command.affectedComponents);
     });
 
     return [];
