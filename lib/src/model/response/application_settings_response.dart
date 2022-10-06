8bc6b44769091fa175fa17cfc712b821b6ad8673
Toni Heiss <toni.heiss@sibvisions.com>
2022-10-06 13:20:13 +0200

    Color parsing

--- lib/src/model/response/application_settings_response.dart
+++ lib/src/model/response/application_settings_response.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import '../../../util/parse_util.dart';
 import '../../service/api/shared/api_object_property.dart';
 import '../../service/api/shared/api_response_names.dart';
 import 'api_response.dart';
@@ -21,7 +22,46 @@ class ApplicationSettingsResponse extends ApiResponse {
 
   /// map with typeName and color as string
   /// e.g. mandatoryBackground, readOnlyBackground, invalidEditorBackground, alternateBackground, ...
-  Map<String, Color> colors = {};
+  late final Color? background;
+  late final Color? alternateBackground;
+  late final Color? foreground;
+  late final Color? activeSelectionBackground;
+  late final Color? activeSelectionForeground;
+  late final Color? inactiveSelectionBackground;
+  late final Color? inactiveSelectionForeground;
+  late final Color? mandatoryBackground;
+  late final Color? readOnlyBackground;
+  late final Color? invalidEditorBackground;
+
+  /// The static constant for control background color. */
+  static const String BACKGROUND = "background";
+
+  /// The static constant for control alternate background color. */
+  static const String ALTERNATE_BACKGROUND = "alternateBackground";
+
+  /// The static constant for control foreground color. */
+  static const String FOREGROUND = "foreground";
+
+  /// The static constant for control active selection background color. */
+  static const String ACTIVE_SELECTION_BACKGROUND = "activeSelectionBackground";
+
+  /// The static constant for control active selection foreground color. */
+  static const String ACTIVE_SELECTION_FOREGROUND = "activeSelectionForeground";
+
+  /// The static constant for control inactive selection background color. */
+  static const String INACTIVE_SELECTION_BACKGROUND = "inactiveSelectionBackground";
+
+  /// The static constant for control inactive selection foreground color. */
+  static const String INACTIVE_SELECTION_FOREGROUND = "inactiveSelectionForeground";
+
+  /// The static constant for control mandatory background color. */
+  static const String MANDATORY_BACKGROUND = "mandatoryBackground";
+
+  /// The static constant for control read only background color. */
+  static const String READ_ONLY_BACKGROUND = "readOnlyBackground";
+
+  /// The static constant for control read only background color. */
+  static const String INVALID_EDITOR_BACKGROUND = "invalidEditorBackground";
 
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Initialization
@@ -37,7 +77,16 @@ class ApplicationSettingsResponse extends ApiResponse {
         homeVisible = true,
         logoutVisible = true,
         userSettingsVisible = true,
-        colors = {},
+        background = null,
+        alternateBackground = null,
+        foreground = null,
+        activeSelectionBackground = null,
+        activeSelectionForeground = null,
+        inactiveSelectionBackground = null,
+        inactiveSelectionForeground = null,
+        mandatoryBackground = null,
+        readOnlyBackground = null,
+        invalidEditorBackground = null,
         super.fromJson(pJson: {ApiObjectProperty.name: ApiResponseNames.applicationSettings}, originalRequest: "");
 
   ApplicationSettingsResponse.fromJson({required Map<String, dynamic> pJson, required Object originalRequest})
@@ -50,13 +99,30 @@ class ApplicationSettingsResponse extends ApiResponse {
         homeVisible = pJson[ApiObjectProperty.home] ?? true,
         logoutVisible = pJson[ApiObjectProperty.logout] ?? true,
         userSettingsVisible = pJson[ApiObjectProperty.userSettings] ?? true,
-        super.fromJson(pJson: pJson, originalRequest: originalRequest) {
-    // dynamic x = pJson[ApiObjectProperty.colors];
-    // for (dynamic y in x) {
-    //   log(y.toString());
-    // }
+        super.fromJson(
+          pJson: pJson,
+          originalRequest: originalRequest,
+        ) {
+    applyColors(pJson[ApiObjectProperty.colors] ?? {});
+  }
 
-    // log(x.toString());
-    //colors = pJson[ApiObjectProperty.colors] ?? {};
+  void applyColors(Map<String, dynamic> pJson) {
+    background = getColor(pJson[BACKGROUND]);
+    alternateBackground = getColor(pJson[ALTERNATE_BACKGROUND]);
+    foreground = getColor(pJson[FOREGROUND]);
+    activeSelectionBackground = getColor(pJson[ACTIVE_SELECTION_BACKGROUND]);
+    activeSelectionForeground = getColor(pJson[ACTIVE_SELECTION_FOREGROUND]);
+    inactiveSelectionBackground = getColor(pJson[INACTIVE_SELECTION_BACKGROUND]);
+    inactiveSelectionForeground = getColor(pJson[INACTIVE_SELECTION_FOREGROUND]);
+    mandatoryBackground = getColor(pJson[MANDATORY_BACKGROUND]);
+    readOnlyBackground = getColor(pJson[READ_ONLY_BACKGROUND]);
+    invalidEditorBackground = getColor(pJson[INVALID_EDITOR_BACKGROUND]);
+  }
+
+  Color? getColor(dynamic pValue) {
+    String? sColor = pValue?.toString();
+
+    sColor = sColor?.split(";").first;
+    return ParseUtil.parseHexColor(sColor);
   }
 }
