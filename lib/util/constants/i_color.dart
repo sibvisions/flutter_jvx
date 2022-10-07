10a81952880c4402ac32805959fd380d37c42ddc
Toni Heiss <toni.heiss@sibvisions.com>
2022-10-07 15:57:31 +0200

    removed unused server colors

--- lib/util/constants/i_color.dart
+++ lib/util/constants/i_color.dart
@@ -5,30 +5,6 @@ import 'package:flutter/widgets.dart';
 abstract class IColorConstants {
   static const Color JVX_LIGHTER_BLACK = Color(0xFF424242);
   static const Color COMPONENT_DISABLED = Color(0xFFBDBDBD);
-
-  static const Color ICONTROL_BACKGROUND = Color(0xFFFFFFFF);
-  static const Color ICONTROL_ALTERNATEBACKGROUND = Color(0xFFFFFFFF);
-  static const Color ICONTROL_FOREGROUND = Color(0xFFFFFFFF);
-  static const Color ICONTROL_ACTIVESELECTIONBACKGROUND = Color(0xFFFFFFFF);
-  static const Color ICONTROL_ACTIVESELECTIONFOREGROUND = Color(0xFFFFFFFF);
-  static const Color ICONTROL_INACTIVESELECTIONBACKGROUND = Color(0xFFFFFFFF);
-  static const Color ICONTROL_INACTIVESELECTIONFOREGROUND = Color(0xFFFFFFFF);
-  static const Color ICONTROL_MANDATORYBACKGROUND = Color(0xFFFFFFFF);
-  static const Color ICONTROL_READONLYBACKGROUND = Color(0xFFFFFFFF);
-  static const Color ICONTROL_INVALIDEDITORBACKGROUND = Color(0xFFFFFFFF);
-
-  static const Map<String, Color> SERVER_COLORS = {
-    "icontrol_background": ICONTROL_BACKGROUND,
-    "icontrol_alternateBackground": ICONTROL_ALTERNATEBACKGROUND,
-    "icontrol_foreground": ICONTROL_FOREGROUND,
-    "icontrol_activeSelectionBackground": ICONTROL_ACTIVESELECTIONBACKGROUND,
-    "icontrol_activeSelectionForeground": ICONTROL_ACTIVESELECTIONFOREGROUND,
-    "icontrol_inactiveSelectionBackground": ICONTROL_INACTIVESELECTIONBACKGROUND,
-    "icontrol_inactiveSelectionForeground": ICONTROL_INACTIVESELECTIONFOREGROUND,
-    "icontrol_mandatoryBackground": ICONTROL_MANDATORYBACKGROUND,
-    "icontrol_readOnlyBackground": ICONTROL_READONLYBACKGROUND,
-    "icontrol_invalidEditorBackground": ICONTROL_INVALIDEDITORBACKGROUND
-  };
 }
 
 abstract class IColor extends IColorConstants {
