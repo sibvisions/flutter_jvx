e517b0c2b3ec22623a3cacc015dbd2da8db698f5
rjahn <rene.jahn@sibvisions.com>
2022-10-12 16:44:05 +0200

    cleanup fixed

--- creator/cleanup.sh
+++ creator/cleanup.sh
@@ -2,10 +2,14 @@ flutterBin=`which flutter`
 flutterDir=`dirname $flutterBin`
 
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/wakelock-0.6.2
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/sqflite-2.0.3+1
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/mobile_scanner-2.0.0
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/device_info_plus-4.1.2
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/package_info_plus-1.4.3+1
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/shared_preferences_ios-2.1.1
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/url_launcher_ios-6.0.17
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/path_provider_ios-2.0.11
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/file_saver-0.1.1
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/image_picker_ios-0.8.6+1
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/file_picker-5.2.1
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/device_info_plus-5.0.5
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/flutter_native_splash-2.2.11
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/sqflite-2.1.0+1
