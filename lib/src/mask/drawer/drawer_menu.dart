e44e4ee46ef3044144543f62442dbc21165b0159
Bungeefan <severin.hamader@yahoo.de>
2022-10-18 13:16:47 +0200

    Improved small drawer menu

--- lib/src/mask/drawer/drawer_menu.dart
+++ lib/src/mask/drawer/drawer_menu.dart
@@ -60,47 +60,61 @@ class _DrawerMenuState extends State<DrawerMenu> {
   Widget _buildDrawerHeader(BuildContext context, bool isNormalSize) {
     var profileImage = IConfigService().getUserInfo()?.profileImage;
 
-    List<Widget> headerItems = [
-      _buildHeaderText(
-        flex: 5,
-        text: AppStyle.of(context)!.applicationStyle!['login.title'] ?? IConfigService().getAppName()!,
-        context: context,
-        constraints: const BoxConstraints(maxWidth: 150),
-        fontWeight: FontWeight.bold,
-      ),
-      const Spacer(flex: 1),
-      Expanded(
-        flex: 4,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildHeaderText(
-              flex: 1,
-              text: "${FlutterJVx.translate("Logged in as")}:",
-              context: context,
-            ),
-            const Padding(padding: EdgeInsets.symmetric(vertical: 1)),
-            _buildHeaderText(
-              flex: 2,
-              text: IConfigService().getUserInfo()?.displayName ?? " ",
-              context: context,
-              fontWeight: FontWeight.bold,
-            ),
-          ],
+    List<Widget> headerItems;
+
+    if (isNormalSize) {
+      headerItems = [
+        _buildHeaderText(
+          flex: 5,
+          text: AppStyle.of(context)!.applicationStyle!['login.title'] ?? IConfigService().getAppName()!,
+          context: context,
+          fontWeight: FontWeight.bold,
         ),
-      ),
-    ];
+        const Spacer(flex: 1),
+        Expanded(
+          flex: 4,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildHeaderText(
+                flex: 1,
+                text: "${FlutterJVx.translate("Logged in as")}:",
+                context: context,
+              ),
+              const Padding(padding: EdgeInsets.symmetric(vertical: 1)),
+              _buildHeaderText(
+                flex: 2,
+                text: IConfigService().getUserInfo()?.displayName ?? " ",
+                context: context,
+                fontWeight: FontWeight.bold,
+              ),
+            ],
+          ),
+        ),
+      ];
+    } else {
+      headerItems = [
+        Flexible(
+          child: _buildHeaderText(
+            text: IConfigService().getUserInfo()?.displayName ?? " ",
+            context: context,
+            constraints: const BoxConstraints(maxWidth: 120),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ];
+    }
 
     return SizedBox(
       height: isNormalSize ? 170 : 100,
       child: DrawerHeader(
         margin: EdgeInsets.zero,
-        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+        padding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 12.0),
         decoration: BoxDecoration(color: Theme.of(context).primaryColor),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Expanded(
-              flex: 6,
               child: isNormalSize
                   ? Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -110,29 +124,22 @@ class _DrawerMenuState extends State<DrawerMenu> {
                       children: headerItems,
                     ),
             ),
-            if (isNormalSize)
-              Expanded(
-                flex: 4,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      child: CircleAvatar(
-                        backgroundColor: Theme.of(context).backgroundColor,
-                        backgroundImage: profileImage != null ? MemoryImage(profileImage) : null,
-                        child: profileImage == null
-                            ? FaIcon(
-                                FontAwesomeIcons.solidUser,
-                                color: Colors.grey.shade400,
-                                size: 60,
-                              )
-                            : null,
-                      ),
-                    )
-                  ],
-                ),
-              )
+            const Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+            AspectRatio(
+              aspectRatio: 1.0,
+              child: CircleAvatar(
+                radius: 100,
+                backgroundColor: Theme.of(context).backgroundColor,
+                backgroundImage: profileImage != null ? MemoryImage(profileImage) : null,
+                child: profileImage == null
+                    ? FaIcon(
+                        FontAwesomeIcons.solidUser,
+                        color: Colors.grey.shade400,
+                        size: 60,
+                      )
+                    : null,
+              ),
+            )
           ],
         ),
       ),
@@ -204,31 +211,35 @@ class _DrawerMenuState extends State<DrawerMenu> {
   /// Flex value changes size according to all other header texts(Expanded).
   /// Text will size itself to fill available space(Fitted-Box).
   Widget _buildHeaderText({
-    required int flex,
+    int? flex,
     required String text,
     required BuildContext context,
     FontWeight? fontWeight,
     BoxConstraints? constraints,
   }) {
-    return Expanded(
-      flex: flex,
-      child: Container(
-        constraints: constraints,
-        child: FittedBox(
-          alignment: Alignment.centerLeft,
-          child: Container(
-            constraints: constraints,
-            child: Text(
-              text,
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.onPrimary,
-                fontWeight: fontWeight,
-              ),
-            ),
+    Widget child = Container(
+      constraints: constraints,
+      width: double.infinity,
+      child: FittedBox(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+            fontWeight: fontWeight,
           ),
         ),
       ),
     );
+
+    if (flex != null) {
+      child = Expanded(
+        flex: flex,
+        child: child,
+      );
+    }
+
+    return child;
   }
 
   /// Build an entry used in the footer of the drawer.
