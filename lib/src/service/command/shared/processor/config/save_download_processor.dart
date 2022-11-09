4ff4e55ec615301b4c6fb1f742aee9be2d50e799
Toni Heiss <toni.heiss@sibvisions.com>
2022-11-09 14:51:19 +0100

    Ticket :: FS#3072 - export in web

--- lib/src/service/command/shared/processor/config/save_download_processor.dart
+++ lib/src/service/command/shared/processor/config/save_download_processor.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:file_saver/file_saver.dart';
+import 'package:flutter/foundation.dart';
 import 'package:path/path.dart' as path;
 
 import '../../../../../model/command/base_command.dart';
@@ -10,19 +11,30 @@ import '../../i_command_processor.dart';
 class SaveDownloadCommandProcessor implements ICommandProcessor<SaveDownloadCommand> {
   @override
   Future<List<BaseCommand>> processCommand(SaveDownloadCommand command) async {
-    String extension = path.extension(command.fileName);
-    if (extension.startsWith(".")) {
-      extension = extension.substring(1);
+    if (kIsWeb) {
+      // https://github.com/incrediblezayed/file_saver/issues/42
+      // File saver ignores the extension property so we have to include it in the name itself
+      unawaited(
+        FileSaver.instance.saveFile(
+          path.basename(command.fileName),
+          command.bodyBytes,
+          "",
+        ),
+      );
+    } else {
+      String extension = path.extension(command.fileName);
+      if (extension.startsWith(".")) {
+        extension = extension.substring(1);
+      }
+      unawaited(
+        FileSaver.instance.saveAs(
+          path.basenameWithoutExtension(command.fileName),
+          command.bodyBytes,
+          "extension",
+          MimeType.OTHER,
+        ),
+      );
     }
-
-    unawaited(
-      FileSaver.instance.saveAs(
-        path.basenameWithoutExtension(command.fileName),
-        command.bodyBytes,
-        extension,
-        MimeType.OTHER,
-      ),
-    );
 
     return [];
   }
