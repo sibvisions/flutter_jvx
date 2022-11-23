10289c222fd3e72df68dc01752638591e2708b9f
rjahn <rene.jahn@sibvisions.com>
2022-11-23 08:47:54 +0100

    removed exact version numbers

--- creator/cleanup.sh
+++ creator/cleanup.sh
@@ -1,16 +1,16 @@
 flutterBin=`which flutter`
 flutterDir=`dirname $flutterBin`
 
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/wakelock-0.6.2
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/mobile_scanner-2.0.0
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/package_info_plus-3.0.1
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/shared_preferences_ios-2.1.1
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/url_launcher_ios-6.0.17
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/path_provider_ios-2.0.11
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/file_saver-0.1.1
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/image_picker_ios-0.8.6+1
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/file_picker-5.2.2
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/device_info_plus-8.0.0
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/flutter_native_splash-2.2.13
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/sqflite-2.2.0+3
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/flutter_keyboard_visibility-5.4.0
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/wakelock-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/mobile_scanner-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/package_info_plus-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/shared_preferences_ios-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/url_launcher_ios-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/path_provider_ios-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/file_saver-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/image_picker_ios-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/file_picker-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/device_info_plus-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/flutter_native_splash-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/sqflite-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/flutter_keyboard_visibility-*
