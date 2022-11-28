b7bb2c966a74d86203b3b8d8839506cbd991433a
Toni Heiss <toni.heiss@sibvisions.com>
2022-11-28 16:33:01 +0100

    removed unnecessary if

--- lib/src/util/extensions/list_extensions.dart
+++ lib/src/util/extensions/list_extensions.dart
@@ -1,8 +1,6 @@
 extension JVxListExtension on List {
   /// Uses [List.removeRange] to remove all objects starting from [start] in this list except the last one.
   void removeAllExceptLast([int start = 0]) {
-    if (length > 1) {
-      removeRange(start, length - 1);
-    }
+    removeRange(start, length - 1);
   }
 }
