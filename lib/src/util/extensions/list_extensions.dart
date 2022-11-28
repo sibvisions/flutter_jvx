3a9725f663cb11627a1fad52f96858c7c05112ff
Toni Heiss <toni.heiss@sibvisions.com>
2022-11-28 16:51:35 +0100

    Revert "removed unnecessary if"
    This reverts commit b7bb2c966a74d86203b3b8d8839506cbd991433a.

--- lib/src/util/extensions/list_extensions.dart
+++ lib/src/util/extensions/list_extensions.dart
@@ -1,6 +1,8 @@
 extension JVxListExtension on List {
   /// Uses [List.removeRange] to remove all objects starting from [start] in this list except the last one.
   void removeAllExceptLast([int start = 0]) {
-    removeRange(start, length - 1);
+    if (length > 1) {
+      removeRange(start, length - 1);
+    }
   }
 }
