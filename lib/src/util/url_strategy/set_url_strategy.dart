8d8d22ec189a06ff9d873fefe1c6957fa596015f
Bungeefan <severin.hamader@yahoo.de>
2022-12-05 16:51:33 +0100

    Fixed flutter_web_plugins import

--- lib/src/util/url_strategy/set_url_strategy.dart
+++ lib/src/util/url_strategy/set_url_strategy.dart
@@ -1,4 +1,4 @@
-import 'set_url_strategy_stub.dart' if (dart.library.html) 'set_url_strategy_impl.dart' as url_strategy;
+import 'set_url_strategy_stub.dart' if (dart.library.html) 'set_url_strategy_web.dart' as url_strategy;
 
 void fixUrlStrategy() {
   url_strategy.setHashUrlStrategy();
