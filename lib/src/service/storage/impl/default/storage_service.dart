362abf530b27f4d5ac5af7034b48fc638acde3b0
Toni Heiss <72435900+ToniHeiss@users.noreply.github.com>
2022-12-13 15:54:27 +0100

    fixed server side - parent remove on desktop panel

--- lib/src/service/storage/impl/default/storage_service.dart
+++ lib/src/service/storage/impl/default/storage_service.dart
@@ -83,7 +83,6 @@ class StorageService implements IStorageService {
         }
         if (componentModel.className == FlContainerClassname.DESKTOP_PANEL) {
           newDesktopPanel = componentModel;
-          componentModel.parent = null;
         }
         _componentMap[componentModel.id] = componentModel;
         _addAsChild(componentModel);
