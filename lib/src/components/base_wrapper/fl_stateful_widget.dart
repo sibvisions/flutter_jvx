127cfa86e5586a2c9066f0c606b97f931b7e3752
Toni Heiss <toni.heiss@sibvisions.com>
2022-12-30 17:31:36 +0100

    Create fl_stateful_widget.dart

---
+++ lib/src/components/base_wrapper/fl_stateful_widget.dart
@@ -0,0 +1,35 @@
+/* 
+ * Copyright 2022 SIB Visions GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import 'package:flutter/widgets.dart';
+
+import '../../model/component/fl_component_model.dart';
+
+/// The base class for all FlutterJVx's components.
+abstract class FlStatefulWidget<T extends FlComponentModel> extends StatefulWidget {
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Class members
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  // The model of the component as sent by the server.
+  final T model;
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Initialization
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  const FlStatefulWidget({super.key, required this.model});
+}
