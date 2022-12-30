e90e078d1d1b2bb37678023ff6c0fdf5936eff76
Toni Heiss <toni.heiss@sibvisions.com>
2022-12-30 17:32:08 +0100

    Supported version 2.2.0 -> 2.1.0

--- lib/src/flutter_ui.dart
+++ lib/src/flutter_ui.dart
@@ -78,7 +78,7 @@ class FlutterUI extends StatefulWidget {
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   /// Determines the supported server version which will be sent in the [ApiStartUpRequest]
-  static const String supportedServerVersion = "2.2.0";
+  static const String supportedServerVersion = "2.1.0";
 
   /// Loads assets with packages prefix
   static bool package = true;
