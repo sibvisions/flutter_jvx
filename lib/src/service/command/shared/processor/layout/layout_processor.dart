8d457c8fc166fedc331e135eca1d4dc8c62c1e61
Toni Heiss <toni.heiss@sibvisions.com>
2022-12-30 18:45:50 +0100

    Remove layout command processors

--- lib/src/service/command/shared/processor/layout/layout_processor.dart
+++ lib/src/service/command/shared/processor/layout/layout_processor.dart
@@ -18,24 +18,20 @@ import '../../../../../model/command/base_command.dart';
 import '../../../../../model/command/layout/layout_mode_command.dart';
 import '../../../../../model/command/layout/preferred_size_command.dart';
 import '../../../../../model/command/layout/register_parent_command.dart';
+import '../../../../../model/response/device_status_response.dart';
+import '../../../../config/config_service.dart';
+import '../../../../layout/i_layout_service.dart';
 import '../../i_command_processor.dart';
-import 'layout_mode_command_processor.dart';
-import 'preferred_size_command_processor.dart';
-import 'register_parent_command_processor.dart';
 
 class LayoutProcessor implements ICommandProcessor {
-  final PreferredSizeCommandProcessor _preferredSizeProcessor = PreferredSizeCommandProcessor();
-  final RegisterParentCommandProcessor _registerParentCommand = RegisterParentCommandProcessor();
-  final LayoutModeCommandProcessor _layoutModeProcessor = LayoutModeCommandProcessor();
-
   @override
   Future<List<BaseCommand>> processCommand(BaseCommand command) async {
     if (command is PreferredSizeCommand) {
-      return _preferredSizeProcessor.processCommand(command);
+      return ILayoutService().reportPreferredSize(pLayoutData: command.layoutData);
     } else if (command is RegisterParentCommand) {
-      return _registerParentCommand.processCommand(command);
+      return ILayoutService().reportLayout(pLayoutData: command.layoutData);
     } else if (command is LayoutModeCommand) {
-      return _layoutModeProcessor.processCommand(command);
+      ConfigService().getLayoutModeNotifier().value = command.layoutMode ?? LayoutMode.Small;
     }
 
     return [];
