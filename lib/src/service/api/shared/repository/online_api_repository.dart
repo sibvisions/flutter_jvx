787bfd20482caada85eaeceba05a3c4f01f38476
Bungeefan <severin.hamader@yahoo.de>
2023-01-13 12:13:16 +0100

    Added socket-readTimeout workaround for iOS, refs #71

--- lib/src/service/api/shared/repository/online_api_repository.dart
+++ lib/src/service/api/shared/repository/online_api_repository.dart
@@ -212,6 +212,8 @@ class OnlineApiRepository implements IRepository {
 
   JVxWebSocket? jvxWebSocket;
 
+  bool get connected => _connected;
+
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Initialization
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
