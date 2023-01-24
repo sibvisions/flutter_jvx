e8dc1a822c0c252c8a0d569b969391767474f1df
rjahn <rene.jahn@sibvisions.com>
2023-01-24 10:46:59 +0100

    changed dependency names

--- creator/cleanup.sh
+++ creator/cleanup.sh
@@ -4,7 +4,7 @@ flutterDir=`dirname $flutterBin`
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/wakelock-*
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/mobile_scanner-*
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/package_info_plus-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/shared_preferences_ios-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/shared_preferences_foundation-*
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/url_launcher_ios-*
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/path_provider_ios-*
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/file_saver-*
@@ -15,3 +15,4 @@ rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/flutter_native_splash-*
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/sqflite-*
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/flutter_keyboard_visibility-*
 rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/flutter_native_timezone-*
+rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/connectivity_plus-*
