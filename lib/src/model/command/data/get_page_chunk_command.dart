c757b71c9f1ba103e0804c7145b4d0bcc4d20369
Toni Heiss <toni.heiss@sibvisions.com>
2023-02-01 15:47:26 +0100

    Create get_page_chunk_command.dart

---
+++ lib/src/model/command/data/get_page_chunk_command.dart
@@ -0,0 +1,65 @@
+/* 
+ * Copyright 2022 SIB Visions GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import '../../data/subscriptions/data_subscription.dart';
+import 'data_command.dart';
+
+class GetPageChunkCommand extends DataCommand {
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Class members
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  /// Id of the [DataSubscription] requesting data
+  final String subId;
+
+  /// Key of the page that is being requested
+  final String pageKey;
+
+  /// Link to the dataBook containing the data
+  final String dataProvider;
+
+  /// From which index data is being requested
+  final int from;
+
+  /// To which index data is being requested
+  final int? to;
+
+  /// True if the the data should only overwrite old existing data
+  final bool isUpdate;
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Initialization
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  GetPageChunkCommand({
+    required this.pageKey,
+    required this.dataProvider,
+    required this.from,
+    required this.subId,
+    this.isUpdate = false,
+    this.to,
+    required super.reason,
+  });
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Overridden methods
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  @override
+  String toString() {
+    return "GetPageChunkCommand{subId: $subId, dataProvider: $dataProvider, from: $from, to: $to, isUpdate: $isUpdate, ${super.toString()}}";
+  }
+}
