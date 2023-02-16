9f5c94d05331f1a674beba56e693bd7e7bf09964
Bungeefan <severin.hamader@yahoo.de>
2023-02-16 18:47:58 +0100

    Implemented initial websocket retry

--- lib/src/service/api/shared/repository/jvx_web_socket.dart
+++ lib/src/service/api/shared/repository/jvx_web_socket.dart
@@ -7,6 +7,7 @@ import 'package:web_socket_channel/status.dart' as status;
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 import '../../../../flutter_ui.dart';
+import '../../../../util/external/retry.dart';
 import '../../../../util/import_handler/import_handler.dart';
 
 /// Uses [WebSocketChannel] to create a web socket for sending and receiving.
@@ -104,95 +105,104 @@ class JVxWebSocket {
     }
 
     try {
-      FlutterUI.logAPI.i("${_logPrefix}Connecting to $uri");
-
-      var webSocket = _webSocket = createWebSocket(
-        uri,
-        headersSupplier?.call(),
-      );
-
-      await webSocket.ready.then((value) {
-        _availability.value = true;
-        if (!_connectedState.value) {
-          // onReady
-          FlutterUI.logAPI.i("${_logPrefix}Connected to WebSocket#${webSocket.hashCode}");
-
-          _connectedState.value = true;
-          _retryDelay = 2;
-          _manualClose = false;
-        }
-
-        onConnectedChange?.call(true);
-      });
-
-      _subscription = webSocket.stream.listen(
-        (data) {
-          // Re-set to possibly override a single failing http request.
-          onConnectedChange?.call(true);
-
-          if (data.isNotEmpty) {
-            try {
-              FlutterUI.logAPI.d("${_logPrefix}Received data via WebSocket#${webSocket.hashCode}: $data");
-              onData.call(data);
-            } catch (e, stack) {
-              FlutterUI.logAPI.e("${_logPrefix}Error handling websocket message:", e, stack);
-            }
-          }
-        },
-        onError: (error) {
-          // As there is no cancel of a currently connecting websocket (yet),
-          // this is only triggered when the connection websocket fails to initially connect.
-          FlutterUI.logAPI.w("${_logPrefix}Connection to WebSocket#${webSocket.hashCode} failed", error);
-
-          _connectedState.value = false;
-
-          if (error is WebSocketChannelException &&
-              error.inner is WebSocketChannelException &&
-              (error.inner as WebSocketChannelException).inner is WebSocketException) {
-            // Server probably doesn't support web sockets.
-            _availability.value = false;
-            FlutterUI.logAPI
-                .i("${_logPrefix}Connection to WebSocket#${webSocket.hashCode} was determined as unavailable");
-          } else {
-            _reconnectWebSocket();
-          }
-        },
-        onDone: () {
-          FlutterUI.logAPI.w(
-            "${_logPrefix}Connection to WebSocket#${webSocket.hashCode} closed ${_manualClose ? "manually " : ""}"
-            "(${webSocket.closeCode ?? "No CloseCode"})"
-            "${webSocket.closeReason?.isNotEmpty ?? false ? ": ${webSocket.closeReason}" : ""}",
-          );
-
-          _connectedState.value = false;
-          _retryDelay = 2;
-
-          if (webSocket.closeCode != status.policyViolation) {
-            // Invalid session shouldn't trigger a offline warning.
-            //
-            // The following scenario can happen:
-            // Connection gets interrupted (trying reconnect)
-            // Server restarts in the meantime (Session gets invalid)
-            // Connections gets restored but clientId is invalid
-            // Connection is ready but Web Socket gets closed because of invalid session
-            onConnectedChange?.call(false);
-          }
-
-          // Don't retry if server goes down because the clientId will be invalid anyway, which triggers a restart on its own.
-          // Don't retry if we closed the socket (indicated either trough manualClose or status.normalClosure)
-          if (!_manualClose &&
-              ![status.normalClosure, status.goingAway, status.policyViolation].contains(webSocket.closeCode)) {
-            _reconnectWebSocket();
-          }
-
-          _manualClose = false;
-        },
-        cancelOnError: true,
+      await retry(
+        maxAttempts: 3,
+        onRetry: (e) => FlutterUI.logAPI.w("${_logPrefix}Retrying initial WebSocket connection", e),
+        () => _connect(uri),
       );
     } catch (e) {
       FlutterUI.logAPI.e("${_logPrefix}Connection to WebSocket could not be established!", e);
       rethrow;
     }
+  }
+
+  Future<void> _connect(Uri uri) async {
+    FlutterUI.logAPI.i("${_logPrefix}Connecting to $uri");
+
+    var webSocket = createWebSocket(
+      uri,
+      headersSupplier?.call(),
+    );
+
+    await webSocket.ready.then((value) {
+      _availability.value = true;
+      if (!_connectedState.value) {
+        // onReady
+        FlutterUI.logAPI.i("${_logPrefix}Connected to WebSocket#${webSocket.hashCode}");
+
+        _connectedState.value = true;
+        _retryDelay = 2;
+        _manualClose = false;
+      }
+
+      onConnectedChange?.call(true);
+    });
+
+    _webSocket = webSocket;
+    _subscription = webSocket.stream.listen(
+      (data) {
+        // Re-set to possibly override a single failing http request.
+        onConnectedChange?.call(true);
+
+        if (data.isNotEmpty) {
+          try {
+            FlutterUI.logAPI.d("${_logPrefix}Received data via WebSocket#${webSocket.hashCode}: $data");
+            onData.call(data);
+          } catch (e, stack) {
+            FlutterUI.logAPI.e("${_logPrefix}Error handling websocket message:", e, stack);
+          }
+        }
+      },
+      onError: (error) {
+        // As there is no cancel of a currently connecting websocket (yet),
+        // this is only triggered when the connection websocket fails to initially connect.
+        FlutterUI.logAPI.w("${_logPrefix}Connection to WebSocket#${webSocket.hashCode} failed", error);
+
+        _connectedState.value = false;
+
+        if (error is WebSocketChannelException &&
+            error.inner is WebSocketChannelException &&
+            (error.inner as WebSocketChannelException).inner is WebSocketException) {
+          // Server probably doesn't support web sockets.
+          _availability.value = false;
+          FlutterUI.logAPI
+              .i("${_logPrefix}Connection to WebSocket#${webSocket.hashCode} was determined as unavailable");
+        } else {
+          _reconnectWebSocket();
+        }
+      },
+      onDone: () {
+        FlutterUI.logAPI.w(
+          "${_logPrefix}Connection to WebSocket#${webSocket.hashCode} closed ${_manualClose ? "manually " : ""}"
+          "(${webSocket.closeCode ?? "No CloseCode"})"
+          "${webSocket.closeReason?.isNotEmpty ?? false ? ": ${webSocket.closeReason}" : ""}",
+        );
+
+        _connectedState.value = false;
+        _retryDelay = 2;
+
+        if (webSocket.closeCode != status.policyViolation) {
+          // Invalid session shouldn't trigger a offline warning.
+          //
+          // The following scenario can happen:
+          // Connection gets interrupted (trying reconnect)
+          // Server restarts in the meantime (Session gets invalid)
+          // Connections gets restored but clientId is invalid
+          // Connection is ready but Web Socket gets closed because of invalid session
+          onConnectedChange?.call(false);
+        }
+
+        // Don't retry if server goes down because the clientId will be invalid anyway, which triggers a restart on its own.
+        // Don't retry if we closed the socket (indicated either trough manualClose or status.normalClosure)
+        if (!_manualClose &&
+            ![status.normalClosure, status.goingAway, status.policyViolation].contains(webSocket.closeCode)) {
+          _reconnectWebSocket();
+        }
+
+        _manualClose = false;
+      },
+      cancelOnError: true,
+    );
   }
 
   void _reconnectWebSocket() {
