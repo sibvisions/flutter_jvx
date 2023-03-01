c392675cdfcad4f85e90cf909de9f922578c399a
rjahn <rene.jahn@sibvisions.com>
2023-03-01 22:58:09 +0100

    Update cleanup.sh
    flutter 3.7 path changes recognized

--- creator/cleanup.sh
+++ creator/cleanup.sh
@@ -1,18 +1,25 @@
 flutterBin=`which flutter`
 flutterDir=`dirname $flutterBin`
 
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/wakelock-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/mobile_scanner-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/package_info_plus-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/shared_preferences_foundation-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/url_launcher_ios-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/path_provider_ios-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/file_saver-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/image_picker_ios-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/file_picker-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/device_info_plus-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/flutter_native_splash-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/sqflite-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/flutter_keyboard_visibility-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/flutter_native_timezone-*
-rm -r $flutterDir/../.pub-cache/hosted/pub.dartlang.org/connectivity_plus-*
+pubCache=$flutterDir/../.pub-cache
+
+if [ ! -d $pubCache ]; then
+  pubCache=~/.pub-cache
+fi
+
+rm -r $pubCache/hosted/pub.dev/wakelock-*
+rm -r $pubCache/hosted/pub.dev/mobile_scanner-*
+rm -r $pubCache/hosted/pub.dev/package_info_plus-*
+rm -r $pubCache/hosted/pub.dev/shared_preferences_foundation-*
+rm -r $pubCache/hosted/pub.dev/url_launcher_ios-*
+rm -r $pubCache/hosted/pub.dev/image_picker_ios-*
+rm -r $pubCache/hosted/pub.dev/file_picker-*
+rm -r $pubCache/hosted/pub.dev/device_info_plus-*
+rm -r $pubCache/hosted/pub.dev/flutter_native_splash-*
+rm -r $pubCache/hosted/pub.dev/sqflite-*
+rm -r $pubCache/hosted/pub.dev/flutter_keyboard_visibility-*
+rm -r $pubCache/hosted/pub.dev/flutter_native_timezone-*
+rm -r $pubCache/hosted/pub.dev/connectivity_plus-*
+rm -r $pubCache/hosted/pub.dev/path_provider_foundation-*
+#rm -r $pubCache/hosted/pub.dev/file_saver-*
+rm -r $pubCache/git/fix_file_saver-*
