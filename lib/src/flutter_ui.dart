65fab244181314f88d6db385e1ec780e9b0fb1d3
Bungeefan <severin.hamader@yahoo.de>
2023-03-08 14:14:50 +0100

    Set supportedServerVersion to 3.1.0

--- lib/src/flutter_ui.dart
+++ lib/src/flutter_ui.dart
@@ -87,7 +87,7 @@ class FlutterUI extends StatefulWidget {
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   /// Determines the supported server version which will be sent in the [ApiStartUpRequest]
-  static const String supportedServerVersion = "3.0.0";
+  static const String supportedServerVersion = "3.1.0";
 
   /// Loads assets with packages prefix
   static bool package = true;
