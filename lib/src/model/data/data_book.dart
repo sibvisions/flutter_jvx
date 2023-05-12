78d848c98ab38e71cf339beb97bde2baf7ffbef9
Toni Heiss <toni.heiss@sibvisions.com>
2023-05-12 10:41:34 +0200

    fixed datachunk to bug -> max changed to min

--- lib/src/model/data/data_book.dart
+++ lib/src/model/data/data_book.dart
@@ -342,7 +342,7 @@ class DataBook {
     int indexOfColumn = metaData.columnDefinitions.indexWhere((element) => element.name == pColumnName);
 
     HashMap<int, List<dynamic>> dataMap = pPageKey != null ? (pageRecords[pPageKey] ?? HashMap()) : records;
-    pTo = max(pTo ?? 0, dataMap.length);
+    pTo = min(pTo ?? 0, dataMap.length);
     for (int i = pFrom; i < pTo; i++) {
       var a = dataMap[i];
       if (a != null) {
