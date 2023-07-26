flutter-preview/sibvisions-flutter_jvx

(Web) Added baseUrl calculation including hidden startup attempt in case baseUrl is missing

lib/src/service/api/shared/repository/online_api_repository.dart
@@ -501,7 +501,7 @@ class OnlineApiRepository extends IRepository {
       return null;
     }
 
-    Uri location = IConfigService().baseUrl.value!;
+    Uri location = _getBaseUrl();
 
     int? end = location.path.lastIndexOf(ParseUtil.urlSuffix);
     if (end == -1) end = null;
@@ -515,6 +515,29 @@ class OnlineApiRepository extends IRepository {
         "reconnect": true.toString(),
       },
     );
+  }
+
+  /// Returns the effective base url.
+  ///
+  /// If there is a calculated base url (only in web, if there is no default app)
+  /// return this url on the first start, otherwise the one from the config.
+  ///
+  /// Background info:
+  /// Flutter Web should be start-able without configuring it nor providing
+  /// a baseUrl and appName parameter in the url.
+  /// Currently only VisionX and deployed version need this.
+  ///
+  /// Throws if there is no url.
+  Uri _getBaseUrl() {
+    Uri? location = IConfigService().baseUrl.value;
+    // On first start, check with calculated url if possible.
+    if (location == null) {
+      if (AppService().beforeFirstStart) {
+        location = AppService().calculatedBaseUrl;
+      }
+    }
+    location = location!;
+    return location;
   }
 
   void setConnectedStatus(bool connected) {
@@ -709,7 +732,7 @@ class OnlineApiRepository extends IRepository {
       throw Exception("URI belonging to ${pRequest.runtimeType} not found, add it to the apiConfig!");
     }
 
-    Uri uri = Uri.parse("${IConfigService().baseUrl.value!}/${route.route}");
+    Uri uri = Uri.parse("${_getBaseUrl()}/${route.route}");
     List<Cookie>? requestCookies;
     if (!kIsWeb) {
       requestCookies = List.of(_cookies);

lib/src/service/command/shared/processor/config/save_application_meta_data_command_processor.dart
@@ -27,6 +27,7 @@ import '../../../../../model/command/base_command.dart';
 import '../../../../../model/command/config/save_application_meta_data_command.dart';
 import '../../../../api/i_api_service.dart';
 import '../../../../api/shared/repository/online_api_repository.dart';
+import '../../../../apps/app.dart';
 import '../../../../apps/app_service.dart';
 import '../../../../config/i_config_service.dart';
 import '../../../../file/file_manager.dart';
@@ -38,6 +39,22 @@ class SaveApplicationMetaDataCommandProcessor implements ICommandProcessor<SaveA
   Future<List<BaseCommand>> processCommand(SaveApplicationMetaDataCommand command) async {
     // Remove '.' to allow easy saving of images in filesystem
     String version = command.metaData.version.replaceAll(".", "_");
+
+    if (IConfigService().currentApp.value == null) {
+      // In case we did a startup without a currentApp, here we are fixing this.
+      Uri? calculatedBaseUrl = AppService().calculatedBaseUrl;
+      String? applicationName = command.metaData.applicationName;
+      if (applicationName != null && calculatedBaseUrl != null) {
+        App calculatedApp =
+            (await App.getApp(App.computeId(applicationName, calculatedBaseUrl.toString(), predefined: false)!)) ??
+                await App.createApp(name: applicationName, baseUrl: calculatedBaseUrl);
+        await IConfigService().updateCurrentApp(calculatedApp.id);
+      } else {
+        // Problem we can't fix it but can't continue too.
+        throw Exception("Failed to create app from calculatedBaseUrl, aborting."
+            "\nCalculatedBaseUrl: $calculatedBaseUrl, ApplicationName: $applicationName");
+      }
+    }
 
     await AppService().removePreviousAppVersions(IConfigService().currentApp.value!, version);
 

sibvisions/flutter_jvx

lib/src/service/api/shared/repository/online_api_repository.dart
@@ -524,7 +524,7 @@ class OnlineApiRepository extends IRepository {
       return null;
     }
 
-    Uri location = IConfigService().baseUrl.value!;
+    Uri location = _getBaseUrl();
 
     int? end = location.path.lastIndexOf(ParseUtil.urlSuffix);
     if (end == -1) end = null;
@@ -538,6 +538,29 @@ class OnlineApiRepository extends IRepository {
         "reconnect": true.toString(),
       },
     );
+  }
+
+  /// Returns the effective base url.
+  ///
+  /// If there is a calculated base url (only in web, if there is no default app)
+  /// return this url on the first start, otherwise the one from the config.
+  ///
+  /// Background info:
+  /// Flutter Web should be start-able without configuring it nor providing
+  /// a baseUrl and appName parameter in the url.
+  /// Currently only VisionX and deployed version need this.
+  ///
+  /// Throws if there is no url.
+  Uri _getBaseUrl() {
+    Uri? location = IConfigService().baseUrl.value;
+    // On first start, check with calculated url if possible.
+    if (location == null) {
+      if (IAppService().beforeFirstStart) {
+        location = IAppService().calculatedBaseUrl;
+      }
+    }
+    location = location!;
+    return location;
   }
 
   void setConnectedStatus(bool connected) {
@@ -602,7 +625,7 @@ class OnlineApiRepository extends IRepository {
         return (error is HttpException && error.message.contains("Connection closed"));
       }
 
-      client!.options.baseUrl = IConfigService().baseUrl.value!.toString();
+      client!.options.baseUrl = _getBaseUrl().toString();
       sendFunction() => _sendRequest(
             pRequest,
             client: client!,

lib/src/service/command/shared/processor/config/save_application_meta_data_command_processor.dart
@@ -27,6 +27,7 @@ import '../../../../../model/command/base_command.dart';
 import '../../../../../model/command/config/save_application_meta_data_command.dart';
 import '../../../../api/i_api_service.dart';
 import '../../../../api/shared/repository/online_api_repository.dart';
+import '../../../../apps/app.dart';
 import '../../../../apps/i_app_service.dart';
 import '../../../../config/i_config_service.dart';
 import '../../../../file/file_manager.dart';
@@ -38,6 +39,22 @@ class SaveApplicationMetaDataCommandProcessor extends ICommandProcessor<SaveAppl
   Future<List<BaseCommand>> processCommand(SaveApplicationMetaDataCommand command, BaseCommand? origin) async {
     // Remove '.' to allow easy saving of images in filesystem
     String version = command.metaData.version.replaceAll(".", "_");
+
+    if (IConfigService().currentApp.value == null) {
+      // In case we did a startup without a currentApp, here we are fixing this.
+      Uri? calculatedBaseUrl = IAppService().calculatedBaseUrl;
+      String? applicationName = command.metaData.applicationName;
+      if (applicationName != null && calculatedBaseUrl != null) {
+        App calculatedApp =
+            (await App.getApp(App.computeId(applicationName, calculatedBaseUrl.toString(), predefined: false)!)) ??
+                await App.createApp(name: applicationName, baseUrl: calculatedBaseUrl);
+        await IConfigService().updateCurrentApp(calculatedApp.id);
+      } else {
+        // Problem we can't fix it but can't continue too.
+        throw Exception("Failed to create app from calculatedBaseUrl, aborting."
+            "\nCalculatedBaseUrl: $calculatedBaseUrl, ApplicationName: $applicationName");
+      }
+    }
 
     await IAppService().removePreviousAppVersions(IConfigService().currentApp.value!, version);
 