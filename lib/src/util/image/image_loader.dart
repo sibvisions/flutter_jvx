6c6c8c08552cdc0c08621c9229256155acf4f944
rjahn <rene.jahn@sibvisions.com>
2024-10-21 16:53:38 +0200

    removed cached_network_image dependency because simple image caching works with network image as well

--- lib/src/util/image/image_loader.dart
+++ lib/src/util/image/image_loader.dart
@@ -18,7 +18,6 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
