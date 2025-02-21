07822da7a35ff08792f1974b979659256c26ff31
rjahn <rene.jahn@sibvisions.com>
2025-02-21 12:04:20 +0100

    fixed build with flutter 3.29

--- creator/cleanup.sh
+++ creator/cleanup.sh
@@ -29,10 +29,12 @@ rm -r $pubCache/hosted/pub.dev/flutter_tts*
 rm -r $pubCache/hosted/pub.dev/speech_to_text*
 rm -r $pubCache/hosted/pub.dev/file_saver*
 rm -r $pubCache/hosted/pub.dev/html_editor_enhanced*
+rm -r $pubCache/git/fix-html_editor_enhanced*
 rm -r $pubCache/hosted/pub.dev/push-*
 rm -r $pubCache/hosted/pub.dev/flutter_local_notifications*
 rm -r $pubCache/hosted/pub.dev/screen_brightness*
 rm -r $pubCache/hosted/pub.dev/open_filex*
+rm -r $pubCache/git/open_filex*
 rm -r $pubCache/hosted/pub.dev/pointer_interceptor*
 rm -r $pubCache/hosted/pub.dev/webview_flutter*
 rm -r $pubCache/hosted/pub.dev/android_id*
