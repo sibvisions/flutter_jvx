376f17648ef89804b4eb6fba39a25fe070e5a64a
rjahn <rene.jahn@sibvisions.com>
2026-02-10 08:33:04 +0100

    API changes

--- lib/src/custom/app_manager.dart
+++ lib/src/custom/app_manager.dart
@@ -108,10 +108,10 @@ abstract class AppManager {
   List<Widget> getAdditionalActions() => [];
 
   /// Can be used to modify the headers for each request
-  void modifyHeaders(ApiRequest request, Map<String, dynamic> headers) {}
+  void modifyHeaders(ApiRequest? request, Map<String, dynamic> headers) {}
 
   /// Can be used to modify the cookie list for each request
-  void modifyCookies(ApiRequest request, List<Cookie> cookies) {}
+  void modifyCookies(ApiRequest? request, List<Cookie> cookies) {}
 
   /// Can be used to modify the application parameters before they get updated via [IUiService.updateApplicationParameters]
   void modifyApplicationParameters(ApplicationParameters pApplicationParameters) {}
