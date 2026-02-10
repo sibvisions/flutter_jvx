4c485b57635dda5b0fdca7cb5d661a23731a43af
rjahn <rene.jahn@sibvisions.com>
2026-02-10 14:20:42 +0100

    support minimal startup (no resources, no websocket)

--- lib/src/model/command/api/startup_command.dart
+++ lib/src/model/command/api/startup_command.dart
@@ -31,6 +31,9 @@ class StartupCommand extends ApiCommand {
   /// This can be used to override the default values from the config.
   final String? password;
 
+  /// Whether to load/init only base resources
+  final bool minimal;
+
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Initialization
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -38,6 +41,7 @@ class StartupCommand extends ApiCommand {
   StartupCommand({
     this.username,
     this.password,
+    this.minimal = false,
     required super.reason,
     super.showLoading,
   });
@@ -48,7 +52,7 @@ class StartupCommand extends ApiCommand {
 
   @override
   String propertiesAsString() {
-    return "username: $username, password: ${password != null ? '<set>' : 'null'}, ${super.propertiesAsString()}";
+    return "username: $username, password: ${password != null ? '<set>' : 'null'}, minimal: $minimal, ${super.propertiesAsString()}";
   }
 
 }
