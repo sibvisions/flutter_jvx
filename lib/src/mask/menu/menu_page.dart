9ef32f75243cf1c08a1dd0ccef0346753a184264
rjahn <rene.jahn@sibvisions.com>
2026-06-17 16:12:14 +0200

    center with row

--- lib/src/mask/menu/menu_page.dart
+++ lib/src/mask/menu/menu_page.dart
@@ -190,22 +190,25 @@ class _MenuPageState extends State<MenuPage> with SearchMixin {
               if (body == null && menuModel.items.isEmpty) {
                 Color colIcon = Theme.of(context).textTheme.labelMedium?.color?.withAlpha(100) ?? Colors.grey;
 
-                body = Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    if (isOffline && !OfflineUtil.isGoingOffline) OfflineUtil.getOfflineBar(context),
-                    const SizedBox(height: 80),
-                    Icon(Icons.notes, size: 80, color: colIcon),
-                    const SizedBox(height: 16),
-                    Text(
-                      FlutterUI.translate("No screens available"),
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: colIcon),
-                    ),
-                  ],
-                );
+                body = Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Column(
+                    mainAxisSize: MainAxisSize.min,
+                      children: [
+                        if (isOffline && !OfflineUtil.isGoingOffline) OfflineUtil.getOfflineBar(context),
+                        const SizedBox(height: 80),
+                        Icon(Icons.notes, size: 80, color: colIcon),
+                        const SizedBox(height: 16),
+                        Text(
+                          FlutterUI.translate("No screens available"),
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: colIcon),
+                        ),
+                      ],
+                  )
+                ]);
               }
 
               Widget? background;
